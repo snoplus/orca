96b174303d44fa488eda5a8d0df285287c2b7efd
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2007-11-19 18:47:32 +0000

    git-svn-id: svn://orca.physics.unc.edu/Orca@131 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Misc/Utilities.h
@@ -0,0 +1,37 @@
+//--------------------------------------------------------------------------------
+/*!\file	Utilities
+ * \brief	This class contains useful functions 
+ * \functions
+ *			\li \b 	convertTimeLongToChar		- Convert standard UNIX time to standard 
+ *												  time string.
+ *			\li \b	convertTimeCharToLong		- Convert standard time string to UNIX
+ *												  time.
+ *			\li \b	ORKeyFromId					- convert an obj pointer value into an NSNumber.
+ * \note	
+ *			
+ * \author	Jan M. Wouters
+ * \history	2003-05-08 (jmw) - Original.
+ */
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+#import <IOKit/IOKitLib.h>
+
+long 	convertTimeCharToLong( char* aTime );
+void	convertTimeLongToChar( time_t aTime, char *pTime );
+id		ORKeyFromId(id anObj);
+int		random_range(int lowest_number, int highest_number);
+io_service_t rootService();
+NSString* listMethods(Class aClass);
+NSString* listMethodWithOptions(Class aClass,BOOL verbose);
