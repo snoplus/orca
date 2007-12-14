4360e9f36fcc86cbad100017d17dde8ad63a0154
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2007-12-14 18:54:01 +0000

    put gradient back in, this time should be compatible with 10.4
    git-svn-id: svn://orca.physics.unc.edu/Orca@293 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Groups/ORGroupView.m
+++ Source/Objects/Groups/ORGroupView.m
@@ -137,9 +137,9 @@
 
 - (void) drawBackground:(NSRect)aRect
 {
-/*	NSRect bounds = [self bounds];
+	NSRect bounds = [self bounds];
 	float red,green,blue,alpha;
-	NSColor* color = [self backgroundColor];
+	NSColor* color = [[self backgroundColor] colorUsingColorSpaceName:NSDeviceRGBColorSpace];
 	[color getRed:&red green:&green blue:&blue alpha:&alpha];
 
 	red *= .75;
@@ -152,12 +152,12 @@
 	CTGradient* gradient = [CTGradient gradientWithBeginningColor:color endingColor:endingColor];
 
 	[gradient fillRect:bounds angle:270.];
-*/
+
 }
 
 - (void)drawRect:(NSRect)rect
 {
-//	[self drawBackground:rect];
+	[self drawBackground:rect];
     [self drawContents:rect];
     [mouseTask drawRect:rect];
 }
