da2be079ee29687677e91733b2fbbb7eda7f5fdb
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2008-03-05 17:07:07 +0000

    new
    git-svn-id: svn://orca.physics.unc.edu/Orca@677 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Objects/Hardware/IPE/IpeV4 Crate/ORIpeV4CrateController.m
@@ -0,0 +1,92 @@
+
+//
+//  ORIpeV4CrateController.m
+//  Orca
+//
+//  Created by Mark Howe on Fri Aug 5, 2005.
+//  Copyright © 2002 CENPA, University of Washington. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+
+#pragma mark •••Imported Files
+#import "ORIpeV4CrateController.h"
+#import "ORIpeFireWireCard.h"
+#import "ORIpeV4CrateModel.h"
+
+#import "ORFireWireInterface.h"
+
+@implementation ORIpeV4CrateController
+
+- (id) init
+{
+    self = [super initWithWindowNibName:@"IpeV4Crate"];
+    return self;
+}
+
+- (void) awakeFromNib
+{
+	[super awakeFromNib];
+	
+	// Set title of crate window, ak 15.6.07
+    [[self window] setTitle:[NSString stringWithFormat:@"IPE-DAQ-V4 Crate %d",[model uniqueIdNumber]]];
+
+}
+
+#pragma mark •••Notifications
+- (void) registerNotificationObservers
+{
+	[super registerNotificationObservers];
+	
+	NSNotificationCenter* notifyCenter = [NSNotificationCenter defaultCenter];   
+	
+    [notifyCenter addObserver : self
+                     selector : @selector(serviceChanged:)
+                         name : ORFireWireInterfaceServiceAliveChanged
+                       object : nil];
+    
+
+    [notifyCenter addObserver : self
+                     selector : @selector(serviceChanged:)
+                         name : ORFireWireInterfaceIsOpenChanged
+                       object : nil];
+	
+    [notifyCenter addObserver : self
+                     selector : @selector(serviceChanged:)
+                         name : ORIpeInterfaceChanged
+                       object : nil];
+
+
+}
+
+
+- (void) updateWindow
+{
+	[super updateWindow];
+	[self serviceChanged:nil];
+}
+
+- (void) serviceChanged:(NSNotification*)aNotification
+{
+	if([[model adapter] serviceIsOpen] && [[model adapter] serviceIsAlive]){
+		[powerField setStringValue:@""];
+	}
+	else {
+		[powerField setStringValue:@"No FW"];
+	}
+	
+}
+
+
+@end
---
+++ Source/Objects/Hardware/IPE/IpeV4 Crate/ORIpeV4CrateModel.m
@@ -0,0 +1,144 @@
+//
+//  ORIpeV4CrateModel.m
+//  Orca
+//
+//  Created by Mark Howe on Fri Aug 5, 2005.
+//  Copyright © 2002 CENPA, University of Washington. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+
+#pragma mark •••Imported Files
+//#import "ORIpeDefs.h"
+#import "ORIpeV4CrateModel.h"
+#import "ORIpeCard.h"
+
+@implementation ORIpeV4CrateModel
+
+#pragma mark •••initialization
+
+- (void) dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+	[super dealloc];
+}
+
+- (void) setUpImage
+{
+    //---------------------------------------------------------------------------------------------------
+    //arghhh....NSImage caches one image. The NSImage setCachMode:NSImageNeverCache appears to not work.
+    //so, we cache the image here so that each crate can have its own version for drawing into.
+    //---------------------------------------------------------------------------------------------------
+    NSImage* aCachedImage = [NSImage imageNamed:@"IpeV4Crate"];
+    NSImage* i = [[NSImage alloc] initWithSize:[aCachedImage size]];
+    [i lockFocus];
+    [aCachedImage compositeToPoint:NSZeroPoint operation:NSCompositeCopy];
+    if(powerOff){
+        NSAttributedString* s = [[[NSAttributedString alloc] initWithString:@"No Pwr"
+                                                                 attributes:[NSDictionary dictionaryWithObjectsAndKeys:
+                                                                     [NSColor redColor],NSForegroundColorAttributeName,
+                                                                     [NSFont fontWithName:@"Geneva" size:10],NSFontAttributeName,
+                                                                     nil]] autorelease]; 
+        [s drawAtPoint:NSMakePoint(25,5)];
+    }
+    
+    if([[self orcaObjects] count]){
+        NSAffineTransform* transform = [NSAffineTransform transform];
+        [transform translateXBy:10 yBy:28];
+        [transform scaleXBy:.5 yBy:.5];
+        [transform concat];
+        NSEnumerator* e  = [[self orcaObjects] objectEnumerator];
+        OrcaObject* anObject;
+        while(anObject = [e nextObject]){
+            BOOL oldHighlightState = [anObject highlighted];
+            [anObject setHighlighted:NO];
+            [anObject drawSelf:NSMakeRect(0,0,500,[[self image] size].height)];
+            [anObject setHighlighted:oldHighlightState];
+        }
+    }
+    [i unlockFocus];
+    [self setImage:i];
+    [i release];
+    
+    [[NSNotificationCenter defaultCenter]
+                postNotificationName:OROrcaObjectImageChanged
+                              object:self];
+							  
+}
+
+- (void) makeMainController
+{
+    [self linkToController:@"ORIpeV4CrateController"];
+}
+
+
+#pragma mark •••Accessors
+
+- (NSString*) adapterArchiveKey
+{
+	return @"Ipe Adapter";
+}
+
+#pragma mark •••Notifications
+- (void) registerNotificationObservers
+{
+    NSNotificationCenter* notifyCenter = [NSNotificationCenter defaultCenter];
+    
+    [notifyCenter addObserver : self
+                     selector : @selector(viewChanged:)
+                         name : ORIpeCardSlotChangedNotification
+                       object : nil];
+    
+}
+
+- (void) findInterface
+{
+	[[self adapter] findInterface];
+}
+
+- (void) adapterChanged:(NSNotification*)aNote
+{
+	if([aNote object] == self){
+		[[self adapter] findInterface];
+	}
+}
+
+
+- (void) checkCards
+{
+	NSEnumerator* e  = [[self orcaObjects] objectEnumerator];
+	ORIpeCard* anObject;
+	while(anObject = [e nextObject]){
+		[anObject checkPresence];
+	}	
+}
+
+- (id)initWithCoder:(NSCoder*)decoder
+{
+    self = [super initWithCoder:decoder];
+    
+	[[self undoManager] disableUndoRegistration];
+        
+	[[self undoManager] enableUndoRegistration];
+	
+	[self registerNotificationObservers];
+    
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder*)encoder
+{
+    [super encodeWithCoder:encoder];
+}
+@end
