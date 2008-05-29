4ebb5af817e4a9cd4df2060e153506ece949f8bf
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2008-05-29 20:53:06 +0000

    new
    git-svn-id: svn://orca.physics.unc.edu/Orca@966 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Objects/Custom Hardware/SNO+/SNORack/ORSNORackController.m
@@ -0,0 +1,165 @@
+
+//
+//  ORSNORackController.m
+//  Orca
+//
+//  Created by Mark Howe on Tue, Apr 30, 2008.
+//  Copyright © 2002 CENPA, University of Washington. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+
+#pragma mark •••Imported Files
+#import "ORSNORackController.h"
+#import "ORSNORackModel.h"
+#import "ORSNOCard.h"
+#import "ORSNOCrateModel.h"
+
+
+@implementation ORSNORackController
+
+- (id) init
+{
+    self = [super initWithWindowNibName:@"SNORack"];
+    return self;
+}
+- (void) awakeFromNib
+{
+	[groupView setGroup:model];
+	[super awakeFromNib];
+}
+#pragma mark •••Notifications
+- (void) registerNotificationObservers
+{
+    [super registerNotificationObservers];
+    NSNotificationCenter* notifyCenter = [NSNotificationCenter defaultCenter];   
+    [notifyCenter addObserver : self
+                     selector : @selector(groupChanged:)
+                         name : ORGroupObjectsAdded
+                       object : nil];
+	
+    [notifyCenter addObserver : self
+                     selector : @selector(groupChanged:)
+                         name : ORGroupObjectsRemoved
+                       object : nil];
+					   
+    [notifyCenter addObserver : self
+                     selector : @selector(groupChanged:)
+                         name : ORGroupSelectionChanged
+                       object : nil];
+	
+    [notifyCenter addObserver : self
+                     selector : @selector(groupChanged:)
+                         name : OROrcaObjectMoved
+                       object : nil];
+	
+	
+    [notifyCenter addObserver : self
+                     selector : @selector(documentLockChanged:)
+                         name : ORDocumentLock
+                        object: nil];
+	
+	[notifyCenter addObserver : self
+					 selector : @selector(documentLockChanged:)
+						 name : ORRunStatusChangedNotification
+					   object : nil];
+ 
+     [notifyCenter addObserver : self
+                     selector : @selector(viewChanged:)
+                         name : ORSNOCrateSlotChanged
+                       object : nil];
+
+	   [notifyCenter addObserver : self
+                     selector : @selector(viewChanged:)
+                         name : ORSNOCardSlotChanged
+                       object : nil];
+}
+
+- (void) viewChanged:(NSNotification*)aNotification
+{
+    [model performSelector:@selector(setUpImage) withObject:nil afterDelay:0];
+}
+
+- (void) rackNumberChanged:(NSNotification*)aNote
+{
+	[self setRackTitle];
+}
+
+- (void) setRackTitle
+{
+	[[self window] setTitle:[NSString stringWithFormat:@"Rack %d",[model uniqueIdNumber]]];
+}
+
+- (void) updateWindow
+{
+    [super updateWindow];
+    [self documentLockChanged:nil];
+	[self viewChanged:nil];
+    [groupView setNeedsDisplay:YES];
+}
+
+- (void) documentLockChanged:(NSNotification*)aNotification
+{
+    if([gSecurity isLocked:ORDocumentLock]) [lockDocField setStringValue:@"Document is locked."];
+    else if([gOrcaGlobals runInProgress])   [lockDocField setStringValue:@"Run In Progress"];
+    else				    [lockDocField setStringValue:@""];
+}
+
+#pragma mark •••Accessors
+- (ORGroupView *)groupView
+{
+    return [self groupView];
+}
+
+- (void) setModel:(id)aModel
+{
+    [super setModel:aModel];
+    [groupView setGroup:(ORGroup*)model];
+	[[self window] setTitle:[NSString stringWithFormat:@"%@",[model identifier]]];
+}
+
+- (void) setCrateTitle
+{
+	[[self window] setTitle:[NSString stringWithFormat:@"SNO crate %d",[model uniqueIdNumber]]];
+}
+
+-(void) groupChanged:(NSNotification*)note
+{
+	if(note == nil || [note object] == model || [[note object] guardian] == model){
+		[model setUpImage];
+		[self updateWindow];
+	}
+}
+
+- (BOOL)validateMenuItem:(NSMenuItem*)menuItem
+{
+	return [groupView validateMenuItem:menuItem];
+}
+
+#pragma mark •••Interface Management
+- (void) slotChanged:(NSNotification*)aNotification
+{
+	[[self window] setTitle:[NSString stringWithFormat:@"%@",[model identifier]]];
+}
+
+//---------------------------------------------------------------
+//these last actions are here only to work around a strange 
+//first responder problem that occurs after cut followed by undo
+- (IBAction)delete:(id)sender   { [groupView delete:sender]; }
+- (IBAction)cut:(id)sender      { [groupView cut:sender]; }
+- (IBAction)paste:(id)sender    { [groupView paste:sender]; }
+- (IBAction)selectAll:(id)sender{ [groupView selectAll:sender]; }
+//-----------------------------------------------------------------
+
+@end
---
+++ Source/Objects/Custom Hardware/SNO+/SNORack/ORSNORackModel.m
@@ -0,0 +1,99 @@
+//
+//  ORSNORackModel.m
+//  Orca
+//
+//  Created by Mark Howe on Tue, Apr 30, 2008.
+//  Copyright © 2002 CENPA, University of Washington. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+#pragma mark •••Imported Files
+#import "ORSNORackModel.h"
+#import "ORSNOCard.h"
+#import "ORSNOCrateModel.h"
+
+@implementation ORSNORackModel
+
+#pragma mark •••initialization
+- (void) makeConnectors
+{	
+}
+
+- (void) setUpImage
+{
+    //---------------------------------------------------------------------------------------------------
+    //arghhh....NSImage caches one image. The NSImage setCachMode:NSImageNeverCache appears to not work.
+    //so, we cache the image here so that each crate can have its own version for drawing into.
+    //---------------------------------------------------------------------------------------------------
+    NSImage* aCachedImage = [NSImage imageNamed:@"SNORack"];
+    NSImage* i = [[NSImage alloc] initWithSize:[aCachedImage size]];
+    [i lockFocus];
+    [aCachedImage compositeToPoint:NSZeroPoint operation:NSCompositeCopy];
+    
+    if([[self orcaObjects] count]){
+        NSAffineTransform* transform = [NSAffineTransform transform];
+        [transform translateXBy:5 yBy:27];
+        [transform scaleXBy:1 yBy:1];
+        [transform concat];
+        NSEnumerator* e  = [[self orcaObjects] objectEnumerator];
+        OrcaObject* anObject;
+        while(anObject = [e nextObject]){
+            BOOL oldHighlightState = [anObject highlighted];
+            [anObject setHighlighted:NO];
+            [anObject drawIcon:NSMakeRect(0,0,500,[aCachedImage size].height) withTransparency:1];
+            [anObject setHighlighted:oldHighlightState];
+        }
+    }
+    [i unlockFocus];
+    [self setImage:i];
+    [i release];
+    
+    [[NSNotificationCenter defaultCenter]
+                postNotificationName:OROrcaObjectImageChanged
+                              object:self];
+}
+
+- (void) makeMainController
+{
+    [self linkToController:@"ORSNORackController"];
+}
+
+
+#pragma mark •••Notifications
+- (void) registerNotificationObservers
+{
+    NSNotificationCenter* notifyCenter = [NSNotificationCenter defaultCenter];
+		   
+    [notifyCenter addObserver : self
+                     selector : @selector(viewChanged:)
+                         name : ORSNOCrateSlotChanged
+                       object : nil];
+
+	   [notifyCenter addObserver : self
+                     selector : @selector(viewChanged:)
+                         name : ORSNOCardSlotChanged
+                       object : nil];
+}
+
+- (void) viewChanged:(NSNotification*)aNotification
+{
+    [self setUpImage];
+}
+
+- (NSString*) identifier
+{
+    return [NSString stringWithFormat:@"SNO Rack %d",[self uniqueIdNumber]];
+}
+
+@end
