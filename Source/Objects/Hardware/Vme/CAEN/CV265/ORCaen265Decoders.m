5bbfdf7514afd9e576eee93e55e26aed446b8b87
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2008-05-29 20:04:15 +0000

    fixed chan name
    git-svn-id: svn://orca.physics.unc.edu/Orca@955 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Hardware/Vme/CAEN/CV265/ORCaen265Decoders.m
+++ Source/Objects/Hardware/Vme/CAEN/CV265/ORCaen265Decoders.m
@@ -77,7 +77,7 @@ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
 	if(IsLongForm(*ptr))ptr++;
     NSString* crate = [NSString stringWithFormat:@"Crate = %d\n",(*ptr&0x01e00000)>>21];
     NSString* card  = [NSString stringWithFormat:@"Card  = %d\n",(*ptr&0x001f0000)>>16];
-    NSString* chan  = [NSString stringWithFormat:@"Card  = %d\n",(*ptr>>13)&0x7];
+    NSString* chan  = [NSString stringWithFormat:@"Chan  = %d\n",(*ptr>>13)&0x7];
 	NSString* type  = [NSString stringWithFormat:@"Range = %@\n",*ptr&0x00001000?@"12 Bit":@"15 Bit"];
 	NSString* data  = [NSString stringWithFormat:@"Value = 0x%x\n",*ptr&0x00000fff];
 	    
