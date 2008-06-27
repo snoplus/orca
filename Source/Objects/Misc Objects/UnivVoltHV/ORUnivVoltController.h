318dffdfbee14e975fc58599c0d5fa552436534c
jwouters <jwouters@fd86f90f-2c0a-422a-b972-84106d8db31b>
2008-06-27 20:46:36 +0000

    First version of Universal Voltronics crate and module added.
    git-svn-id: svn://orca.physics.unc.edu/Orca@1023 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Objects/Misc Objects/UnivVoltHV/ORUnivVoltController.h
@@ -0,0 +1,48 @@
+//
+//  ORUnivVoltController.h
+//  Orca
+//
+//  Created by Jan Wouters on Tues June 24, 2008
+//  Copyright (c) 2008, LANS. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+#import "OrcaObjectController.h"
+
+@interface ORUnivVoltController : OrcaObjectController 
+{
+	IBOutlet NSTextField*	ipConnectedTextField;
+	IBOutlet NSTextField*	ipAddressTextField;
+	IBOutlet NSButton*		ipConnectButton;
+	IBOutlet NSTextField*	frameErrorField;
+	IBOutlet NSTextField*	receiveCountField;
+	IBOutlet NSMatrix*		averageValueMatrix;
+	IBOutlet NSButton*		dialogLock;
+}
+
+#pragma mark ***Interface Management
+- (void) receiveCountChanged:(NSNotification*)aNote;
+- (void) isConnectedChanged:(NSNotification*)aNote;
+- (void) ipAddressChanged:(NSNotification*)aNote;
+- (void) frameErrorChanged:(NSNotification*)aNote;
+- (void) averageChanged:(NSNotification*)aNote;
+- (void) settingsLockChanged:(NSNotification*)aNote;
+
+#pragma mark •••Actions
+- (IBAction) ipAddressTextFieldAction:(id)sender;
+- (IBAction) connectAction:(id)sender;
+- (IBAction) dialogLockAction:(id)sender;
+
+@end
+
---
+++ Source/Objects/Misc Objects/UnivVoltHVCrate/ORUnivVoltHVCrateModel.m
@@ -0,0 +1,245 @@
+//
+//  ORUnivVoltHVCrateModel.m
+//  Orca
+//
+//  Created by Mark Howe on Fri Nov 22 2002.
+//  Copyright © 2002 CENPA, University of Washington. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+
+#pragma mark •••Imported Files
+#import "ORUnivVoltHVCrateModel.h"
+#import "ORCamacCard.h"
+#import "ORCamacBusProtocol.h"
+#import "ORMacModel.h"
+
+NSString* ORUnivVoltHVUSBConnector		    = @"ORUnivVoltHVUSBConnector";
+
+@implementation ORUnivVoltHVCrateModel
+
+#pragma mark •••initialization
+
+- (void) setUpImage
+{
+    //---------------------------------------------------------------------------------------------------
+    //arghhh....NSImage caches one image. The NSImage setCachMode:NSImageNeverCache appears to not work.
+    //so, we cache the image here so that each crate can have its own version for drawing into.
+    //---------------------------------------------------------------------------------------------------
+    NSImage* aCachedImage = [NSImage imageNamed:@"UnivVoltHVCrate"];
+    NSImage* i = [[NSImage alloc] initWithSize:[aCachedImage size]];
+    [i lockFocus];
+    [aCachedImage compositeToPoint:NSZeroPoint operation:NSCompositeCopy];
+    if([self powerOff]){
+        NSAttributedString* s = [[[NSAttributedString alloc] initWithString:@"No Pwr"
+                                                                 attributes:[NSDictionary dictionaryWithObjectsAndKeys:
+                                                                     [NSColor redColor],NSForegroundColorAttributeName,
+                                                                     [NSFont fontWithName:@"Geneva" size:10],NSFontAttributeName,
+                                                                     nil]] autorelease]; 
+        [s drawAtPoint:NSMakePoint(35,10)];
+    }
+    
+    if([[self orcaObjects] count]){
+        NSAffineTransform* transform = [NSAffineTransform transform];
+        [transform translateXBy:20 yBy:40];
+        [transform scaleXBy:.62 yBy:.62];
+        [transform concat];
+        NSEnumerator* e  = [[self orcaObjects] objectEnumerator];
+        OrcaObject* anObject;
+        while(anObject = [e nextObject]){
+            BOOL oldHighlightState = [anObject highlighted];
+            [anObject setHighlighted:NO];
+            [anObject drawSelf:NSMakeRect(0,0,500,[[self image] size].height)];
+            [anObject setHighlighted:oldHighlightState];
+        }
+    }
+    [i unlockFocus];
+    [self setImage:i];
+    [i release];
+    
+    [[NSNotificationCenter defaultCenter]
+                postNotificationName:OROrcaObjectImageChanged
+                              object:self];
+}
+
+- (void) makeMainController
+{
+    [self linkToController:@"ORUnivVoltHVCrateController"];
+}
+
+- (id) usbController
+{
+	return [[self objectConnectedTo:ORUnivVoltHVUSBConnector] getUSBController];
+}
+
+- (void) makeConnectors
+{
+	//since CAMAC can have usb or pci adapters, we let the controllers make the connectors
+}
+
+- (void) connectionChanged
+{
+	ORConnector* controllerConnector = [[self connectors] objectForKey:[self crateAdapterConnectorKey]];
+	ORConnector* usbConnector = [[self connectors] objectForKey:ORUnivVoltHVUSBConnector];
+	if(![usbConnector isConnected] && ![controllerConnector isConnected]){
+		[usbConnector setHidden:NO];
+		[controllerConnector setHidden:NO];
+		if(cratePowerAlarm){
+			[self setPowerOff:NO];
+		    [cratePowerAlarm clearAlarm];
+			[cratePowerAlarm release];
+			cratePowerAlarm = nil;
+			[self viewChanged:nil];
+			[[NSNotificationCenter defaultCenter]
+                postNotificationName:ORForceRedraw
+                              object:self];
+
+		}
+	}
+	else {
+		if([usbConnector isConnected]){
+			usingUSB = YES;
+			[controllerConnector setHidden:YES];
+		}
+		else {
+			usingUSB = NO;
+			[usbConnector setHidden:YES];
+		}
+	}
+}
+
+#pragma mark •••Accessors
+- (NSString*) adapterArchiveKey
+{
+	return @"UnivVoltHV Adapter";
+}
+
+//------------------------------------
+//depreciated (11/29/06) remove someday
+- (NSString*) crateAdapterConnectorKey
+{
+	return @"UnivVoltHV Crate Adapter Connector";
+}
+//------------------------------------
+
+
+#pragma mark •••Notifications
+- (void) registerNotificationObservers
+{
+    NSNotificationCenter* notifyCenter = [NSNotificationCenter defaultCenter];
+    
+	[super registerNotificationObservers];
+	
+    [notifyCenter addObserver : self
+                     selector : @selector(viewChanged:)
+                         name : ORCamacCardSlotChangedNotification
+                       object : nil];
+
+    
+    [notifyCenter addObserver : self
+                     selector : @selector(powerFailed:)
+                         name : @"UnivVoltHVPowerFailedNotification"
+                       object : nil];
+    
+    [notifyCenter addObserver : self
+                     selector : @selector(powerRestored:)
+                         name : @"UnivVoltHVPowerRestoredNotification"
+                       object : nil];
+}
+
+- (void) powerFailed:(NSNotification*)aNotification
+{
+    if([aNotification object] == [self controllerCard] || [[aNotification object] guardian] == self){
+        if(!cratePowerAlarm){
+            cratePowerAlarm = [[ORAlarm alloc] initWithName:@"No UnivVoltHV Crate Power" severity:kHardwareAlarm];
+            [cratePowerAlarm setSticky:YES];
+            [cratePowerAlarm setHelpStringFromFile:@"NoUnivVoltHVCratePowerHelp"];
+            [cratePowerAlarm postAlarm];
+        } 
+        [self setPowerOff:YES];
+    }
+}
+
+- (void) powerRestored:(NSNotification*)aNotification
+{
+    if([aNotification object] == [self controllerCard] || [[aNotification object] guardian] == self){
+        if(cratePowerAlarm){
+            [cratePowerAlarm clearAlarm];
+            [cratePowerAlarm release];
+            cratePowerAlarm = nil;
+		}
+		
+		if([self powerOff])[[self adapter] executeZCycle];
+        [self setPowerOff:NO];
+    }
+}
+
+- (void) pollCratePower
+{
+	if(usingUSB){
+		[NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(pollCratePower) object:nil];
+		NS_DURING
+			//if(![[ORGlobal sharedInstance] runInProgress]){
+			[[self controllerCard] checkCratePower];
+			//}
+		NS_HANDLER
+		NS_ENDHANDLER
+		[self performSelector:@selector(pollCratePower) withObject:nil afterDelay:10];
+	}
+	else [super pollCratePower];
+}
+
+
+- (id) controllerCard
+{
+	if(usingUSB){
+		return [self adapter];
+	}
+	else {
+		return [self objectConnectedTo:[self crateAdapterConnectorKey]];
+	}
+}
+
+- (void) runAboutToStart:(NSNotification*)aNote
+{
+	[[self controllerCard] checkCratePower];
+	
+    [[self adapter] setCrateInhibit:YES];
+    NSLog(@"Inhibit Set on UnivVoltHV Crate %d\n",[self crateNumber]);
+    //[[self adapter] executeZCycle];
+    //NSLog(@"Z-Cycle UnivVoltHV Crate %d\n",[self crateNumber]);
+    //[[self adapter] executeCCycle];
+    //NSLog(@"C-Cycle UnivVoltHV Crate %d\n",[self crateNumber]);
+    
+}
+
+- (void) runStarted:(NSNotification*)aNote
+{
+    [[self adapter] setCrateInhibit:NO];
+    NSLog(@"Inhibit Released on UnivVoltHV Crate %d\n",[self crateNumber]);
+}
+
+- (void) runAboutToStop:(NSNotification*)aNote
+{
+    [[self adapter] setCrateInhibit:YES];    
+    NSLog(@"Inhibit Set on UnivVoltHV Crate %d\n",[self crateNumber]);
+}
+
+- (void) doNoPowerAlert:(NSException*)exception action:(NSString*)message
+{
+    NSLogColor([NSColor redColor],@"****** CC32 Not Available          ******\n");
+	[super doNoPowerAlert:exception action:message];
+}
+
+@end
