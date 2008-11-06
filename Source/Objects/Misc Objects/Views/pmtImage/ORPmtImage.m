03b664c691198373cb66263027b6841edc139012
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2008-11-06 20:57:59 +0000

    new
    git-svn-id: svn://orca.physics.unc.edu/Orca@1276 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Objects/Misc Objects/Views/pmtImage/ORPmtImage.m
@@ -0,0 +1,74 @@
+//
+//  ORPmtImage.m
+//  Orca
+//
+//  Created by Mark Howe on Fri Oct 22, 2004.
+//  Copyright � 2002 CENPA, University of Washington. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+
+#import "ORPmtImage.h"
+
+static NSImage *pmtImage, *colorMaskImage, *topImage;
+
+@implementation ORPmtImage
++ (void)initialize 
+{
+	pmtImage         =  [[NSImage imageNamed:@"pmt"] retain];
+	colorMaskImage   =  [[NSImage imageNamed:@"pmtColorMask"] retain];
+	topImage		 =	[[NSImage imageNamed:@"pmtShadow"] retain];
+}
+
+
++ (ORPmtImage *)pmtWithColor:(NSColor *)aColor angle:(float)anAngle
+{
+     return [[[self alloc] initWithColor:aColor angle:anAngle] autorelease];
+}
+
+- (id) initWithColor:(NSColor*)aColor angle:(float)anAngle
+{
+    if (self = [super initWithSize:[pmtImage size]]) {
+		if(!pmtImage)		pmtImage         =  [[NSImage imageNamed:@"pmt"] retain];
+		if(!colorMaskImage)	colorMaskImage   =  [[NSImage imageNamed:@"pmtColorMask"] retain];
+		if(!topImage)		topImage		 =	[[NSImage imageNamed:@"pmtShadow"] retain];
+		angle = anAngle;
+		[self setColor:aColor];
+    }
+    return self;
+}
+
+- (void) setColor:(NSColor*)aColor
+{
+	NSImage *anImage = [colorMaskImage copy];        
+	NSRect imageBounds = NSMakeRect(0, 0, [anImage size].width, [anImage size].height); 
+	[anImage lockFocus];
+	[aColor set];
+	NSRectFillUsingOperation(imageBounds, NSCompositeSourceAtop);
+	[anImage unlockFocus];
+
+	anImage = [anImage copy];        
+	[anImage lockFocus];
+	[pmtImage compositeToPoint:NSZeroPoint operation:NSCompositeSourceOver];
+	[anImage compositeToPoint:NSZeroPoint operation:NSCompositeSourceOver fraction:1];
+	[topImage compositeToPoint:NSZeroPoint operation:NSCompositeSourceOver fraction:.6];
+	[anImage unlockFocus];
+	
+	NSImage* newImage =     [self rotateIndividualImage: anImage angle:angle];
+	[self lockFocus];
+	[newImage compositeToPoint:NSZeroPoint operation:NSCompositeSourceOver fraction:1];
+	[self unlockFocus];
+}
+
+@end
