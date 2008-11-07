03bd028455edefc31797b3df025476eac4fadcf0
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2008-11-07 14:39:08 +0000

    new
    git-svn-id: svn://orca.physics.unc.edu/Orca@1285 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Objects/Misc Objects/USB/SupportedUSBDevices.h
@@ -0,0 +1,36 @@
+/*
+ *  SupportedUSBDevices.h
+ *  Orca
+ *
+ *  Created by Mark Howe on 11/7/08.
+ *  Copyright 2008 University of North Carolina. All rights reserved.
+ *
+ */
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+//If adding a device, don't forget to increment the following
+#define kNumberSupportedDevices 4
+
+static struct supportedUSbDevices {
+	unsigned long vendorID;
+	unsigned long productID;
+	NSString*	  modelName;
+} supportedUSBDevice[kNumberSupportedDevices] = {
+	{ 0x0957,	0x0407,	@"ORPulser332220Model"	},
+	{ 0x16DC,	0x1,	@"ORCCUSBModel"			},
+	{ 0x0a07,	0x00C8,	@"ORADU200Model"		},
+	{ 0x0403,	0x6001,	@"ORUSBtoBPICModel"		}
+};
+
