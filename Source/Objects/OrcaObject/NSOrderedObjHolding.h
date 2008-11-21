8e44a0a45205f9a37ba81700d76d28286bd92e56
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2008-11-21 13:22:55 +0000

    new
    git-svn-id: svn://orca.physics.unc.edu/Orca@1366 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Objects/OrcaObject/NSOrderedObjHolding.h
@@ -0,0 +1,39 @@
+/*
+ *  OROrderedObjHolding.h
+ *  Orca
+ *
+ *  Created by Mark Howe on 11/19/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+@protocol OROrderedObjHolding
+
+- (int) maxNumberOfCards;
+- (int) cardWidth;
+- (int) groupSeparation;
+- (NSRange) legalSlotsForCard:(id)aCard;
+- (BOOL) slot:(int)aSlot excludedFor:(id)aCard;
+- (int) stationForSlot:(int)aSlot;
+- (int) slotAtPoint:(NSPoint)aPoint;
+- (NSPoint) pointForSlot:(int)aSlot;
+- (void) place:(id)aCard intoSlot:(int)aSlot;
+- (BOOL) slot:(int)aSlot excludedFor:(id)aCard;
+- (NSArray*)selectedObjects;
+- (NSEnumerator*) objectEnumerator;
+@end
+
+
