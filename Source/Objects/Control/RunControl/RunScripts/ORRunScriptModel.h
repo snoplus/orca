f9dffc35056aac997baa605ae9050239ca2c7c76
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2008-12-31 16:50:29 +0000

    git-svn-id: svn://orca.physics.unc.edu/Orca@1450 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Control/RunControl/RunScripts/ORRunScriptModel.h
+++ Source/Objects/Control/RunControl/RunScripts/ORRunScriptModel.h
@@ -17,62 +17,24 @@
 //for the use of this software.
 //-------------------------------------------------------------
 
-//-------------------------------------------------------------------------
-
 #pragma mark ***Imported Files
 #import "ORBaseDecoder.h"
+#import "ORScriptIDEModel.h"
 
-@class ORScriptRunner;
-@class ORDataPacket;
 @class ORDataSet;
-@class ORLineNumberingTextStorage;
 
-#define kRunStartScript 0
-#define kRunStopScript  1
-
-@interface ORRunScriptModel : OrcaObject
-{
-	NSString*				script;
-	ORScriptRunner*			scriptRunner;
-	NSString*				scriptName;
-	BOOL					parsedOK;
-	BOOL					scriptExists;
-	NSString*				lastFile;
+@interface ORRunScriptModel : ORScriptIDEModel
+{	
 	unsigned long		    dataId;
-    BOOL					showSuperClass;
-	NSMutableArray*			inputValues;
-    NSString*				comments;
 	SEL						selectorOK;
 	SEL						selectorBAD;
 	id						anArg;
 	id						target;
 }
 
-
-#pragma mark ***Initialization
-- (id) init;
-- (void) dealloc;
-
-#pragma mark ***Accessors
+#pragma mark ***Scripting
+- (void) scriptRunnerDidFinish:(BOOL)normalFinish returnValue:(id)aValue;
 - (void) setSelectorOK:(SEL)aSelectorOK bad:(SEL)aSelectorBAD withObject:(id)anObject target:(id)aTarget;
-- (NSString*) comments;
-- (void)	setComments:(NSString*)aComments;
-- (BOOL)	showSuperClass;
-- (void)	setShowSuperClass:(BOOL)aShowSuperClass;
-- (NSString*) lastFile;
-- (void) setLastFile:(NSString*)aFile;
-- (NSString*) script;
-- (void) setScript:(NSString*)aString;
-- (void) setScriptNoNote:(NSString*)aString;
-- (NSString*) scriptName;
-- (void) setScriptName:(NSString*)aString;
-- (BOOL) parsedOK;
-- (BOOL) scriptExists;
-- (ORScriptRunner*) scriptRunner;
-- (NSMutableArray*) inputValues;
-- (void) addInputValue;
-- (void) removeInputValue:(int)i;
-- (NSString*) identifier;
 
 #pragma mark ***Data ID
 - (unsigned long) dataId;
@@ -82,29 +44,10 @@
 - (NSDictionary*) dataRecordDescription;
 - (void) shipTaskRecord:(id)aTask running:(BOOL)aState;
 
-#pragma mark ***Script Methods
-- (void) scriptRunnerDidFinish:(BOOL)normalFinish returnValue:(id)aValue;
-- (void) parseScript;
-- (void) runScript;
-- (BOOL) running;
-- (void) stopScript;
-- (void) saveFile;
-- (void) loadScriptFromFile:(NSString*)aFilePath;
-- (void) saveScriptToFile:(NSString*)aFilePath;
-
 #pragma mark •••Archival
 - (id)initWithCoder:(NSCoder*)decoder;
 - (void)encodeWithCoder:(NSCoder*)encoder;
 @end
-
-extern NSString* ORRunScriptModelCommentsChanged;
-extern NSString* ORRunScriptLock;
-extern NSString* ORRunScriptModelShowSuperClassChanged;
-extern NSString* ORRunScriptScriptChanged;
-extern NSString* ORRunScriptNameChanged;
-extern NSString* ORRunScriptArgsChanged;
-extern NSString* ORRunScriptLastFileChangedChanged;
-
 
 @interface ORDecoderRunScript : ORBaseDecoder
 {}
--- Source/Objects/Control/RunControl/RunScripts/ORRunScriptModel.m
+++ Source/Objects/Control/RunControl/RunScripts/ORRunScriptModel.m
@@ -20,42 +20,18 @@
 //-------------------------------------------------------------------------
 
 #pragma mark ***Imported Files
-#import <Cocoa/Cocoa.h>
 #import "ORRunScriptModel.h"
-#import "ORScriptRunner.h"
 #import "ORDataTypeAssigner.h"
 #import "ORDataPacket.h"
 #import "ORDataSet.h"
 
-NSString* ORRunScriptModelCommentsChanged = @"ORRunScriptModelCommentsChanged";
-NSString* ORRunScriptModelShowSuperClassChanged = @"ORRunScriptModelShowSuperClassChanged";
-NSString* ORRunScriptScriptChanged			= @"ORRunScriptScriptChanged";
-NSString* ORRunScriptNameChanged			= @"ORRunScriptNameChanged";
-NSString* ORRunScriptArgsChanged			= @"ORRunScriptArgsChanged";
-NSString* ORRunScriptLastFileChangedChanged = @"ORRunScriptLastFileChangedChanged";
-NSString* ORRunScriptLock					= @"ORRunScriptLock";
-
 @implementation ORRunScriptModel
 
 #pragma mark ***Initialization
-- (id) init 
-{
-    self = [super init];
-    return self;
-}
 
-- (void) dealloc 
+- (void) makeMainController
 {
-    [comments release];
-	[scriptName release];
-	[inputValues release];
-	[scriptRunner release];
-    [super dealloc];
-}
-
--(void)makeMainController
-{
-    [self linkToController:@"ORRunScriptController"];
+    [self linkToController:@"ORScriptIDEController"];
 }
 
 - (void) connectionChanged
@@ -65,15 +41,15 @@ NSString* ORRunScriptLock					= @"ORRunScriptLock";
 
 - (void) setUpImage
 {
-   // [self setImage:[NSImage imageNamed:@"RunScript"]];
-
+	// [self setImage:[NSImage imageNamed:@"RunScript"]];
+	
     //---------------------------------------------------------------------------------------------------
     //arghhh....NSImage caches one image. The NSImage setCachMode:NSImageNeverCache appears to not work.
     //so, we cache the image here so we can draw into it.
     //---------------------------------------------------------------------------------------------------
     
     NSImage* aCachedImage = [NSImage imageNamed:@"RunScript"];
-	    
+	
     NSImage* i = [[NSImage alloc] initWithSize:[aCachedImage size]];
     [i lockFocus];
     [aCachedImage compositeToPoint:NSZeroPoint operation:NSCompositeCopy];
@@ -97,113 +73,6 @@ NSString* ORRunScriptLock					= @"ORRunScriptLock";
 	[self setUpImage];
 }
 
-#pragma mark ***Accessors
-
-- (NSString*) comments
-{
-    return comments;
-}
-
-- (void) setComments:(NSString*)aComments
-{
-	if(!aComments)aComments = @"";
-    [[[self undoManager] prepareWithInvocationTarget:self] setComments:comments];
-    
-    [comments autorelease];
-    comments = [aComments copy];    
-
-    [[NSNotificationCenter defaultCenter] postNotificationName:ORRunScriptModelCommentsChanged object:self];
-}
-
-- (BOOL) showSuperClass
-{
-    return showSuperClass;
-}
-
-- (void) setShowSuperClass:(BOOL)aShowSuperClass
-{
-    [[[self undoManager] prepareWithInvocationTarget:self] setShowSuperClass:showSuperClass];
-    
-    showSuperClass = aShowSuperClass;
-
-    [[NSNotificationCenter defaultCenter] postNotificationName:ORRunScriptModelShowSuperClassChanged object:self];
-}
-
-- (NSString*) lastFile
-{
-	return lastFile;
-}
-
-- (void) setLastFile:(NSString*)aFile
-{
-	if(!aFile)aFile = [[NSHomeDirectory() stringByAppendingPathComponent:@"Untitled"] stringByExpandingTildeInPath];
-	[[[self undoManager] prepareWithInvocationTarget:self] setLastFile:lastFile];
-    [lastFile autorelease];
-    lastFile = [aFile copy];		
-	[[NSNotificationCenter defaultCenter] postNotificationName:ORRunScriptLastFileChangedChanged object:self];
-}
-
-- (NSString*) script
-{
-	return script;
-}
-
-- (void) setScript:(NSString*)aString
-{
-	if(!aString)aString= @"";
-    //[[[self undoManager] prepareWithInvocationTarget:self] setScript:script];
-    [script autorelease];
-    script = [aString copy];	
-	[[NSNotificationCenter defaultCenter] postNotificationName:ORRunScriptScriptChanged object:self];
-}
-
-- (void) setScriptNoNote:(NSString*)aString
-{
-    [script autorelease];
-    script = [aString copy];	
-}
-
-- (NSString*) scriptName
-{
-	return scriptName;
-}
-
-- (void) setScriptName:(NSString*)aString
-{
-	if(!aString)aString = @"OrcaScript";
-    [[[self undoManager] prepareWithInvocationTarget:self] setScriptName:scriptName];
-    [scriptName autorelease];
-    scriptName = [aString copy];	
-	[[NSNotificationCenter defaultCenter] postNotificationName:ORRunScriptNameChanged object:self];
-	[self setUpImage];
-}
-
-- (NSString*) identifier
-{
-    return [NSString stringWithFormat:@"%@ %d",[self scriptName],[self uniqueIdNumber]];
-}
-
-- (NSMutableArray*) inputValues
-{
-	return inputValues;
-}
-
-- (void) addInputValue
-{
-	if(!inputValues)inputValues = [[NSMutableArray array] retain];
-	[inputValues addObject:[NSMutableDictionary dictionaryWithObjectsAndKeys:
-							[NSString stringWithFormat:@"$%d",[inputValues count]],	@"name",
-							[NSDecimalNumber numberWithUnsignedLong:0],				@"iValue",
-							nil]];
-	
-}
-
-- (void) removeInputValue:(int)i
-{
-	[inputValues removeObjectAtIndex:i];
-}
-
-
 #pragma mark ***Data ID
 - (unsigned long) dataId { return dataId; }
 - (void) setDataId: (unsigned long) DataId
@@ -224,11 +93,11 @@ NSString* ORRunScriptLock					= @"ORRunScriptLock";
 {
     NSMutableDictionary* dataDictionary = [NSMutableDictionary dictionary];
     NSDictionary* aDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
-        @"ORDecoderRunScript",					@"decoder",
-        [NSNumber numberWithLong:dataId],		@"dataId",
-        [NSNumber numberWithBool:NO],           @"variable",
-        [NSNumber numberWithLong:3],            @"length",
-        nil];
+								 @"ORDecoderRunScript",					@"decoder",
+								 [NSNumber numberWithLong:dataId],		@"dataId",
+								 [NSNumber numberWithBool:NO],           @"variable",
+								 [NSNumber numberWithLong:3],            @"length",
+								 nil];
     [dataDictionary setObject:aDictionary forKey:@"runScript"];
     
     return dataDictionary;
@@ -241,75 +110,17 @@ NSString* ORRunScriptLock					= @"ORRunScriptLock";
     [aDataPacket addDataDescriptionItem:[self dataRecordDescription] forKey:@"ORRunScriptModel"];
     
 }
+
 - (NSMutableDictionary*) addParametersToDictionary:(NSMutableDictionary*)dictionary
 {
-    
     NSMutableDictionary* objDictionary = [NSMutableDictionary dictionary];
-	if(inputValues) [objDictionary setObject:inputValues forKey:@"inputValues"];
-    if(scriptName)  [objDictionary setObject:scriptName forKey:@"scriptName"];
-    if(lastFile) [objDictionary setObject:lastFile forKey:@"lastFile"];
+	if([super inputValues]) [objDictionary setObject:inputValues forKey:@"inputValues"];
+    if([super scriptName])  [objDictionary setObject:scriptName forKey:@"scriptName"];
+    if([super lastFile]) [objDictionary setObject:lastFile forKey:@"lastFile"];
     [dictionary setObject:objDictionary forKey:@"RunScript"];
 	return objDictionary;
 }
 
-#pragma mark ***Script Methods
-- (ORScriptRunner*) scriptRunner
-{
-	return scriptRunner;
-}
-
-- (BOOL) parsedOK
-{
-	return parsedOK;
-}
-
-- (BOOL) scriptExists
-{
-	return scriptExists;
-}
-
-- (void) parseScript
-{
-	parsedOK = YES;
-	if(!scriptRunner)scriptRunner = [[ORScriptRunner alloc] init];
-	if(![scriptRunner running]){
-		[scriptRunner setScriptName:scriptName];
-		[scriptRunner parse:script];
-		parsedOK = [scriptRunner parsedOK];
-		scriptExists = [scriptRunner scriptExists];
-		if(([[NSApp currentEvent] modifierFlags] & 0x80000)>0){
-			//option key is down
-			[scriptRunner printAll];
-		}
-		[scriptRunner release];
-		scriptRunner = nil;
-	}
-}
-
-- (void) runScript
-{
-	parsedOK = YES;
-	if(!scriptRunner)scriptRunner = [[ORScriptRunner alloc] init];
-	if(![scriptRunner running]){
-		[scriptRunner setScriptName:scriptName];
-		[scriptRunner parse:script];
-		parsedOK = [scriptRunner parsedOK];
-		if(parsedOK){
-			if([scriptRunner scriptExists]){
-				[scriptRunner setFinishCallBack:self selector:@selector(scriptRunnerDidFinish:returnValue:)];
-				[scriptRunner run:inputValues sender:self];
-			}
-			else {
-				[self scriptRunnerDidFinish:YES returnValue:[NSNumber numberWithInt:1]];
-			}
-		}
-	}
-	else {
-		[scriptRunner stop];
-		[scriptRunner release];
-		scriptRunner = nil;
-	}
-}
 
 - (void) setSelectorOK:(SEL)aSelectorOK bad:(SEL)aSelectorBAD withObject:(id)anObject target:(id)aTarget
 {
@@ -319,56 +130,20 @@ NSString* ORRunScriptLock					= @"ORRunScriptLock";
 	target		= [aTarget retain];
 }
 
-
 - (void) scriptRunnerDidFinish:(BOOL)normalFinish returnValue:(id)aValue
 {
+	[super scriptRunnerDidFinish:normalFinish returnValue:aValue];
 	if(normalFinish){
-		NSLog(@"[%@] Returned with: %@\n",[self identifier],aValue);
 		if([aValue intValue]!=0) [target performSelector:selectorOK withObject:anArg];
 		else					 [target performSelector:selectorBAD withObject:nil];
 	}
 	else {
-		NSLogColor([NSColor redColor],@"[%@] Abnormal exit!\n",[scriptRunner scriptName]);
 		[target performSelector:selectorBAD withObject:nil];
 	}
 	[anArg release];
 	anArg = nil;
 	[target release];
 	target = nil;
-}
-
-- (void) stopScript
-{
-	[scriptRunner stop];
-	[scriptRunner release];
-	scriptRunner = nil;
-}
-
-- (BOOL) running
-{
-	return [scriptRunner running];
-}
-
-- (void) loadScriptFromFile:(NSString*)aFilePath
-{
-	[self setLastFile:aFilePath];
-	[self setScript:[NSString stringWithContentsOfFile:[lastFile stringByExpandingTildeInPath]]];
-}
-
-- (void) saveFile
-{
-	[self saveScriptToFile:lastFile];
-}
-
-- (void) saveScriptToFile:(NSString*)aFilePath
-{
-	NSFileManager* fm = [NSFileManager defaultManager];
-	if([fm fileExistsAtPath:[aFilePath stringByExpandingTildeInPath]]){
-		[fm removeFileAtPath:[aFilePath stringByExpandingTildeInPath] handler:nil];
-	}
-	NSData* theData = [script dataUsingEncoding:NSASCIIStringEncoding];
-	[fm createFileAtPath:[aFilePath stringByExpandingTildeInPath] contents:theData attributes:nil];
-	[self setLastFile:aFilePath];
 }
 
 #pragma mark •••Archival
@@ -378,26 +153,14 @@ NSString* ORRunScriptLock					= @"ORRunScriptLock";
     
     [[self undoManager] disableUndoRegistration];
 	
-    [self setComments:[decoder decodeObjectForKey:@"ORRunScriptModelComments"]];
-    [self setShowSuperClass:[decoder decodeBoolForKey:@"showSuperClass"]];
-    [self setScript:[decoder decodeObjectForKey:@"script"]];
-    [self setScriptName:[decoder decodeObjectForKey:@"scriptName"]];
-    [self setLastFile:[decoder decodeObjectForKey:@"lastFile"]];
-    inputValues = [[decoder decodeObjectForKey:@"inputValues"] retain];	
     [[self undoManager] enableUndoRegistration];
-		
+	
     return self;
 }
 
 - (void)encodeWithCoder:(NSCoder*)encoder
 {
     [super encodeWithCoder:encoder];
-    [encoder encodeObject:comments forKey:@"ORRunScriptModelComments"];
-    [encoder encodeBool:showSuperClass forKey:@"showSuperClass"];
-    [encoder encodeObject:script forKey:@"script"];
-    [encoder encodeObject:scriptName forKey:@"scriptName"];
-    [encoder encodeObject:inputValues forKey:@"inputValues"];
-    [encoder encodeObject:lastFile forKey:@"lastFile"];
 }
 
 - (void) shipTaskRecord:(id)aTask running:(BOOL)aState
@@ -412,9 +175,7 @@ NSString* ORRunScriptLock					= @"ORRunScriptLock";
 															object:[NSData dataWithBytes:&data length:sizeof(long)*3]];
     }
 }
-
 @end
-
 
 @implementation ORDecoderRunScript
 - (unsigned long) decodeData:(void*)someData  fromDataPacket:(ORDataPacket*)aDataPacket intoDataSet:(ORDataSet*)aDataSet
