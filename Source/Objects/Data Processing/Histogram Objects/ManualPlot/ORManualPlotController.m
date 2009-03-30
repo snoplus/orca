c53bec983537303fce763a79381e9175037b68e3
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2009-03-30 14:49:09 +0000

    moved
    git-svn-id: svn://orca.physics.unc.edu/Orca@1902 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Objects/Data Processing/Histogram Objects/ManualPlot/ORManualPlotController.m
@@ -0,0 +1,280 @@
+
+//
+//  ORManualPlotController.m
+//  Orca
+//
+//  Created by Mark Howe on Fri Apr 27 2009.
+//  Copyright (c) 2009 CENPA, University of Washington. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+#pragma mark •••Imported Files
+#import "ORManualPlotController.h"
+#import "ORManualPlotModel.h"
+#import "ORPlotter1D.h"
+#import "ORCalibration.h"
+
+@interface ORManualPlotController (private)
+- (void) selectWriteFileDidEnd:(NSOpenPanel *)sheet returnCode:(int)returnCode contextInfo:(void  *)contextInfo;
+- (void) _calibrationDidEnd:(id)sheet returnCode:(int)returnCode contextInfo:(id)userInfo;
+@end
+
+@implementation ORManualPlotController
+
+#pragma mark •••Initialization
+- (id) init
+{
+    self = [super initWithWindowNibName:@"ManualPlot"];
+    return self;
+}
+
+- (void) registerNotificationObservers
+{
+    [super registerNotificationObservers];
+    NSNotificationCenter* notifyCenter = [NSNotificationCenter defaultCenter];   
+    
+    [notifyCenter addObserver: self
+                     selector: @selector(dataChanged:)
+                         name: ORManualPlotDataChanged
+                       object: model];
+	
+    [notifyCenter addObserver : self
+                     selector : @selector(colKeyChanged:)
+                         name : ORManualPlotModelColKeyChanged
+						object: model];
+	
+    [notifyCenter addObserver : self
+                     selector : @selector(col0TitleChanged:)
+                         name : ORManualPlotModelCol0TitleChanged
+						object: model];
+	
+    [notifyCenter addObserver : self
+                     selector : @selector(col1TitleChanged:)
+                         name : ORManualPlotModelCol1TitleChanged
+						object: model];
+	
+    [notifyCenter addObserver : self
+                     selector : @selector(col2TitleChanged:)
+                         name : ORManualPlotModelCol2TitleChanged
+						object: model];
+}
+
+- (void) awakeFromNib
+{
+	[plotter setUseGradient:YES];
+	[super awakeFromNib];
+}
+
+- (void) updateWindow
+{
+	[super updateWindow];
+	[self col0TitleChanged:nil];
+	[self col1TitleChanged:nil];
+	[self col2TitleChanged:nil];
+	[self colKeyChanged:nil];
+}
+
+#pragma mark •••Interface Management
+- (void) colKeyChanged:(NSNotification*)aNote
+{
+	[col0KeyPU selectItemAtIndex: [model col0Key]];
+	[col1KeyPU selectItemAtIndex: [model col1Key]];
+	[col2KeyPU selectItemAtIndex: [model col2Key]];
+	[self refreshPlot:nil];
+}
+
+
+- (void) col0TitleChanged:(NSNotification*)aNotification
+{
+	NSString* title = [model col0Title];
+	if(!title)title = @"Col 0";
+	[[[dataTableView tableColumnWithIdentifier:@"0"] headerCell] setTitle:title];
+	[col0LabelField setStringValue:title];
+	[dataTableView reloadData];
+	[self refreshPlot:nil];
+}
+
+- (void) col1TitleChanged:(NSNotification*)aNotification
+{
+	NSString* title = [model col1Title];
+	if(!title)title = @"Col 1";
+	[[[dataTableView tableColumnWithIdentifier:@"1"] headerCell] setTitle:title];
+	[col1LabelField setStringValue:title];
+	[dataTableView reloadData];
+	[self refreshPlot:nil];
+}
+
+- (void) col2TitleChanged:(NSNotification*)aNotification
+{
+	NSString* title = [model col2Title];
+	if(!title)title = @"Col 2";
+	[[[dataTableView tableColumnWithIdentifier:@"2"] headerCell] setTitle:title];
+	[col2LabelField setStringValue:title];
+		
+	[dataTableView reloadData];
+	[self refreshPlot:nil];
+}
+
+- (void) dataChanged:(NSNotification*)aNotification
+{
+	[dataTableView reloadData];
+	[plotter setNeedsDisplay:YES];
+}
+
+- (void) refreshModeChanged:(NSNotification*)aNotification
+{
+	//we don't have refresh modes
+}
+- (void) pausedChanged:(NSNotification*)aNotification
+{
+	//we don't have paused modes
+}
+
+#pragma mark •••Actions
+- (IBAction) refreshPlot:(id)sender
+{
+	int col0Key = [model col0Key];
+	NSString* title;
+	if(col0Key > 2) title = @"Index";
+	else title = [[[dataTableView tableColumnWithIdentifier:[NSString stringWithFormat:@"%d",col0Key]] headerCell] title];
+	[[plotter xScale] setLabel:title];
+	
+	title = @"";
+	int col1Key = [model col1Key];
+	int col2Key = [model col2Key];
+	[y1LengendField setStringValue:@""];
+	[y2LengendField setStringValue:@""];
+	if(col1Key <= 2) {
+		NSString* s = [[[dataTableView tableColumnWithIdentifier:[NSString stringWithFormat:@"%d",col1Key]] headerCell] title];
+		title = [title stringByAppendingString:s];
+		[y1LengendField setStringValue:s];
+	}
+	if(col2Key <= 2) {
+		if([title length])title = [title stringByAppendingString:@" , "];
+		NSString* s = [[[dataTableView tableColumnWithIdentifier:[NSString stringWithFormat:@"%d",col2Key]] headerCell] title];
+		title = [title stringByAppendingString:s];
+		[y2LengendField setStringValue:s];
+	}
+	if([title length]==0)title = @"Index";
+	
+	
+	[y1LengendField setTextColor:[self colorForDataSet:0]];
+	[y2LengendField setTextColor:[self colorForDataSet:1]];
+
+	[[plotter yScale] setLabel:title];
+	[plotter setNeedsDisplay:YES];
+}
+
+- (IBAction) col2KeyAction:(id)sender
+{
+	[model setCol2Key:[sender indexOfSelectedItem]];	
+}
+
+- (IBAction) col1KeyAction:(id)sender
+{
+	[model setCol1Key:[sender indexOfSelectedItem]];	
+}
+
+- (IBAction) col0KeyAction:(id)sender
+{
+	[model setCol0Key:[sender indexOfSelectedItem]];	
+}
+
+- (IBAction) writeDataFileAction:(id)sender
+{
+	NSSavePanel *savePanel = [NSSavePanel savePanel];
+    [savePanel setPrompt:@"Save As"];
+    [savePanel setCanCreateDirectories:YES];
+    
+    [savePanel beginSheetForDirectory:NSHomeDirectory()
+                                 file:@"Untitled"
+                       modalForWindow:[self window]
+                        modalDelegate:self
+                       didEndSelector:@selector(selectWriteFileDidEnd:returnCode:contextInfo:)
+                          contextInfo:NULL];
+	
+}
+- (IBAction) calibrate:(id)sender
+{
+	NSDictionary* aContextInfo = [NSDictionary dictionaryWithObjectsAndKeys: model, @"ObjectToCalibrate",
+								  model , @"ObjectToUpdate",
+								  nil];
+	calibrationPanel = [[ORCalibrationPane calibrateForWindow:[self window] 
+												modalDelegate:self 
+											   didEndSelector:@selector(_calibrationDidEnd:returnCode:contextInfo:)
+												  contextInfo:aContextInfo] retain];
+	
+}
+
+#pragma mark •••Data Source
+- (int)numberOfRowsInTableView:(NSTableView *)tableView
+{
+	return [model numPoints];
+}
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(int)row
+{
+	return [model dataAtRow:row column:[[tableColumn identifier] intValue]];
+}
+
+- (BOOL) useXYPlot
+{
+	return YES;
+}
+
+- (int) numberOfDataSetsInPlot:(id)aPlotter
+{
+	return 2;
+}
+
+- (int) numberOfPointsInPlot:(id)aPlotter dataSet:(int)set
+{
+	return [model numPoints];
+}
+- (BOOL) plotter:(id)aPlotter dataSet:(int)set index:(unsigned long)index x:(float*)xValue y:(float*)yValue
+{
+	return [model dataSet:set index:index x:xValue y:yValue];
+}
+
+- (BOOL)   	willSupplyColors
+{
+    return YES;
+}
+
+- (NSColor*) colorForDataSet:(int)set
+{
+    switch(set){
+        case 0:  return [NSColor redColor];
+        case 1:  return [NSColor blueColor];
+		default: return [NSColor colorWithCalibratedRed:10/255. green:90/255. blue:0 alpha:1];
+    }
+}
+@end
+
+@implementation ORManualPlotController (private)
+- (void) selectWriteFileDidEnd:(NSOpenPanel *)sheet returnCode:(int)returnCode contextInfo:(void  *)contextInfo
+{
+    if(returnCode){
+        [model writeDataToFile:[[sheet filenames] objectAtIndex:0]];
+    }
+}
+
+- (void) _calibrationDidEnd:(id)sheet returnCode:(int)returnCode contextInfo:(id)userInfo
+{
+	[calibrationPanel release];
+	calibrationPanel = nil;
+}
+
+@end
+
---
+++ Source/Objects/Data Processing/Histogram Objects/ManualPlot/ORManualPlotModel.m
@@ -0,0 +1,291 @@
+//
+//  ORManualPlotModel.m
+//  Orca
+//
+//  Created by Mark Howe on Fri Apr 27 2009.
+//  Copyright (c) 2009 CENPA, University of Washington. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+#pragma mark •••Imported Files
+#import "ORManualPlotModel.h"
+#import "NSNotifications+Extensions.h"
+
+NSString* ORManualPlotModelCol2TitleChanged = @"ORManualPlotModelCol2TitleChanged";
+NSString* ORManualPlotModelCol1TitleChanged = @"ORManualPlotModelCol1TitleChanged";
+NSString* ORManualPlotModelCol0TitleChanged = @"ORManualPlotModelCol0TitleChanged";
+NSString* ORManualPlotModelColKeyChanged	= @"ORManualPlotModelColKeyChanged";
+NSString* ORManualPlotLock					= @"ORManualPlotLock";
+NSString* ORManualPlotDataChanged			= @"ORManualPlotDataChanged";
+
+@implementation ORManualPlotModel
+
+#pragma mark •••initialization
+- (id) init 
+{
+    self = [super init];
+	dataSetLock = [[NSLock alloc] init];
+    return self;    
+}
+
+- (void) dealloc
+{
+    [col2Title release];
+    [col1Title release];
+    [col0Title release];
+	[dataSetLock release];
+    [NSObject cancelPreviousPerformRequestsWithTarget:self];
+	[data release];
+    [super dealloc];
+}
+
+- (void) clearData
+{
+	[dataSetLock lock];
+	[data release];
+	data = nil;
+	[dataSetLock unlock];	
+}
+
+- (void) addValue1:(float)v1 value2:(float)v2 value3:(float)v3
+{
+	[dataSetLock lock];
+	if(!data) data = [[NSMutableArray array] retain];
+	[data addObject:[NSArray arrayWithObjects:
+					 [NSNumber numberWithFloat:v1],
+					 [NSNumber numberWithFloat:v2],
+					 [NSNumber numberWithFloat:v3],
+					 nil]];
+	[dataSetLock unlock];	
+	[[NSNotificationCenter defaultCenter] postNotificationOnMainThreadWithName:ORManualPlotDataChanged object:self];
+
+}
+
+#pragma mark ***Accessors
+- (void) postUpdate
+{
+	[[NSNotificationCenter defaultCenter] postNotificationName:ORManualPlotDataChanged object:self];    
+}
+
+- (id) calibration
+{
+	return calibration;
+}
+
+- (void) setCalibration:(id)aCalibration
+{
+	[aCalibration retain];
+	[calibration release];
+	calibration  = aCalibration;
+}
+
+- (NSString*) col2Title
+{
+    return col2Title;
+}
+
+- (void) setCol2Title:(NSString*)aCol2Title
+{
+    [col2Title autorelease];
+    col2Title = [aCol2Title copy];    
+
+    [[NSNotificationCenter defaultCenter] postNotificationName:ORManualPlotModelCol2TitleChanged object:self];
+}
+
+- (NSString*) col1Title
+{
+    return col1Title;
+}
+
+- (void) setCol1Title:(NSString*)aCol1Title
+{
+    [col1Title autorelease];
+    col1Title = [aCol1Title copy];    
+
+    [[NSNotificationCenter defaultCenter] postNotificationName:ORManualPlotModelCol1TitleChanged object:self];
+}
+
+- (NSString*) col0Title
+{
+    return col0Title;
+}
+
+- (void) setCol0Title:(NSString*)aCol0Title
+{
+    [col0Title autorelease];
+    col0Title = [aCol0Title copy];    
+
+    [[NSNotificationCenter defaultCenter] postNotificationName:ORManualPlotModelCol0TitleChanged object:self];
+}
+
+- (int) col2Key;
+{
+    return col2Key;
+}
+
+- (void) setCol2Key:(int)aCol2Key;
+{
+    [[[self undoManager] prepareWithInvocationTarget:self] setCol2Key:col2Key];
+    col2Key = aCol2Key;    
+    [[NSNotificationCenter defaultCenter] postNotificationName:ORManualPlotModelColKeyChanged object:self];
+}
+
+- (int) col1Key;
+{
+    return col1Key;
+}
+
+- (void) setCol1Key:(int)aCol1Key;
+{
+    [[[self undoManager] prepareWithInvocationTarget:self] setCol1Key:col1Key];
+    col1Key = aCol1Key;    
+    [[NSNotificationCenter defaultCenter] postNotificationName:ORManualPlotModelColKeyChanged object:self];
+}
+
+- (int) col0Key
+{
+    return col0Key;
+}
+
+- (void) setCol0Key:(int)aCol0Key;
+{
+    [[[self undoManager] prepareWithInvocationTarget:self] setCol0Key:col0Key];
+    col0Key = aCol0Key;    
+    [[NSNotificationCenter defaultCenter] postNotificationName:ORManualPlotModelColKeyChanged object:self];
+}
+
+- (void) setUpImage
+{
+	[self setImage:[NSImage imageNamed:@"ManualPlot"]];
+}
+
+- (void) makeMainController
+{
+    [self linkToController:@"ORManualPlotController"];
+}
+
+#pragma mark •••Archival
+- (id)initWithCoder:(NSCoder*)decoder
+{
+    self = [super initWithCoder:decoder];
+	
+    [[self undoManager] disableUndoRegistration];
+    [self setCol2Key:[decoder decodeIntForKey:@"ORManualPlotModelCol2Key"]];
+    [self setCol1Key:[decoder decodeIntForKey:@"ORManualPlotModelCol1Key"]];
+    [self setCol0Key:[decoder decodeIntForKey:@"ORManualPlotModelCol0Key"]];
+	[self setCalibration:[decoder decodeObjectForKey:@"calibration"]];
+	if(col0Key==0 && col1Key==0 && col2Key==0){
+		[self setCol0Key:0]; 
+		[self setCol1Key:1];		
+	}
+    [[self undoManager] enableUndoRegistration];
+
+	dataSetLock = [[NSLock alloc] init];
+	
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder*)encoder
+{
+    [super encodeWithCoder:encoder];
+    [encoder encodeInt:col2Key forKey:@"ORManualPlotModelCol2Key"];
+    [encoder encodeInt:col1Key forKey:@"ORManualPlotModelCol1Key"];
+    [encoder encodeInt:col0Key forKey:@"ORManualPlotModelCol0Key"];
+    [encoder encodeObject:calibration forKey:@"calibration"];
+}
+
+-(void)clear
+{
+	[dataSetLock lock];
+	[data release];
+	data = nil;
+	[dataSetLock unlock];
+}
+
+- (NSString*) fullName
+{
+	return [self fullID];
+}
+
+- (NSString*) fullNameWithRunNumber
+{
+	//fake out so we can inherit
+	return [self fullID];
+}
+
+#pragma mark •••Writing Data
+- (void) writeDataToFile:(NSString*)aFileName
+{
+	[dataSetLock lock];
+	
+	NSString* fullFileName = [aFileName stringByExpandingTildeInPath];
+	FILE* aFile = fopen([fullFileName cStringUsingEncoding:NSASCIIStringEncoding],"w"); 
+	if(aFile){
+		NSLog(@"Writing Manual Plot File: %@\n",fullFileName);
+		NSEnumerator* e = [data objectEnumerator];
+		NSArray* row;
+		while(row=[e nextObject]){
+			fprintf(aFile, "%f\t%f\t%f\n",[[row objectAtIndex:0] floatValue],[[row objectAtIndex:1] floatValue],[[row objectAtIndex:2] floatValue]);
+		}
+		fclose(aFile);
+	}
+	
+	[dataSetLock unlock];
+}
+		
+#pragma mark *** delegate methods
+-(id) dataAtRow:(int)r column:(int)c
+{
+	if(r<[data count]){
+		NSArray* row =  [data objectAtIndex:r];
+		if(c<[row count]){
+			return [row objectAtIndex:c];
+		}
+	}
+	return nil;
+}
+
+
+- (unsigned long) numPoints
+{
+    return [data count];
+}
+
+- (BOOL) dataSet:(int)set index:(unsigned long)index x:(float*)xValue y:(float*)yValue
+{
+	[dataSetLock lock];
+	
+	if(index<[data count]){
+		id d = [data objectAtIndex:index];
+		if(col0Key <= 2) *xValue = [[d objectAtIndex:col0Key] floatValue];
+		else *xValue = index;
+		if(set==0) {
+			if(col1Key <= 2) *yValue = [[d objectAtIndex:col1Key] floatValue];
+			else *yValue=0;
+		}
+		else {
+			if(col2Key <= 2) *yValue = [[d objectAtIndex:col2Key] floatValue];
+			else *yValue=0;
+		}
+	}
+	else {
+		*xValue = 0;
+		*yValue = 0;
+	}
+	[dataSetLock unlock];
+    return YES;    
+}
+
+
+@end
