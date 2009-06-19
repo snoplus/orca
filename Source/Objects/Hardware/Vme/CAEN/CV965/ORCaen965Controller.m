61622e6d9e6dfe7c6af0a4caa8a978671ff901b7
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2009-06-19 17:21:11 +0000

    new
    git-svn-id: svn://orca.physics.unc.edu/Orca@2230 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Objects/Hardware/Vme/CAEN/CV965/ORCaen965Controller.m
@@ -0,0 +1,55 @@
+/*
+ *  ORCaen965Controller.m
+ *  Orca
+ *
+ *  Created by Mark Howe on Thurs May 29 2008.
+ *  Copyright (c) 2008 CENPA, University of Washington. All rights reserved.
+ *
+ */
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+#import "ORCaen965Controller.h"
+#import "ORCaenDataDecoder.h"
+#import "ORCaen965Model.h"
+
+@implementation ORCaen965Controller
+
+#pragma mark ***Initialization
+- (id) init
+{
+    self = [ super initWithWindowNibName: @"Caen965" ];
+    return self;
+}
+
+
+
+#pragma mark •••Notifications
+- (void) registerNotificationObservers
+{
+    [ super registerNotificationObservers ];
+}
+
+#pragma mark ***Interface Management
+- (void) updateWindow
+{
+   [ super updateWindow ];
+}
+
+#pragma mark ***Interface Management - Module specific
+- (NSString*) thresholdLockName {return @"ORCaen965ThresholdLock";}
+- (NSString*) basicLockName     {return @"ORCaen965BasicLock";}
+
+#pragma mark •••Actions
+@end
