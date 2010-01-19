0856df3844dbb0336d14ea116f31ff4456f696fe
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2010-01-19 19:39:28 +0000

    fixed icon type error
    git-svn-id: svn://orca.physics.unc.edu/Orca@2937 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/ProcessLogic/ProcessElements/Inputs/BaseClasses/ORInputElement.m
+++ Source/Objects/ProcessLogic/ProcessElements/Inputs/BaseClasses/ORInputElement.m
@@ -157,7 +157,7 @@ NSString* ORInputElementOutConnection  = @"ORInputElementOutConnection";
 		if([self state]) anImage = [NSImage imageNamed:@"greenled"];
 		else             anImage = [NSImage imageNamed:@"redled"];
 	}
-	else if(viewIconType == 1){
+	else {
 		if([self state]) anImage = [NSImage imageNamed:@"OnText"];
 		else             anImage = [NSImage imageNamed:@"OffText"];
 	}
