a6bdc7458019ecef6b87b324185f86612f066ae5
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2010-03-09 16:31:46 +0000

    added cmd-click for opening plots from segments
    git-svn-id: svn://orca.physics.unc.edu/Orca@3140 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Experiments/Katrin/KatrinModel.m
+++ Source/Experiments/Katrin/KatrinModel.m
@@ -208,6 +208,35 @@ static NSString* KatrinDbConnector		= @"KatrinDbConnector";
 	return @"";
 }
 
+
+- (void) showDataSetForSet:(int)aSet segment:(int)index
+{ 
+	if(aSet>=0 && aSet < [segmentGroups count]){
+		ORSegmentGroup* aGroup = [segmentGroups objectAtIndex:aSet];
+		NSString* cardName = [aGroup segment:index objectForKey:@"kCardSlot"];
+		NSString* chanName = [aGroup segment:index objectForKey:@"kChannel"];
+		if(cardName && chanName && ![cardName hasPrefix:@"-"] && ![chanName hasPrefix:@"-"]){
+			ORDataSet* aDataSet = nil;
+			[[[self document] collectObjectsOfClass:NSClassFromString(@"OrcaObject")] makeObjectsPerformSelector:@selector(clearLoopChecked)];
+			NSArray* objs = [[self document] collectObjectsOfClass:NSClassFromString(@"ORRunModel")];
+			if([objs count]){
+				NSArray* arrayOfHistos = [[objs objectAtIndex:0] collectConnectedObjectsOfClass:NSClassFromString(@"ORHistoModel")];
+				if([arrayOfHistos count]){
+					id histoObj = [arrayOfHistos objectAtIndex:0];
+				//	if([[aGroup adcClassName] isEqualToString:@"ORKatrinFLTModel"] || [[aGroup adcClassName] isEqualToString:@"ORIpeV4FLTModel"]){
+						aDataSet = [histoObj objectForKeyArray:[NSMutableArray arrayWithObjects:@"FLT", @"Energy (0 - 65536)", @"Crate  0",
+																[NSString stringWithFormat:@"Station %2d",[cardName intValue]], 
+																[NSString stringWithFormat:@"Channel %2d",[chanName intValue]],
+																nil]];
+			//		}
+					
+					[aDataSet doDoubleClick:nil];
+				}
+			}
+		}
+	}
+}
+
 #pragma mark ¥¥¥Specific Dialog Lock Methods
 - (NSString*) experimentMapLock
 {
