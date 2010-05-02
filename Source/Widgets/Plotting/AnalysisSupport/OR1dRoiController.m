c51270ecdf3144ecfcd0f1bcdebacc5b94b8f2f1
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2010-05-02 18:42:04 +0000

    v9.0'
    git-svn-id: svn://orca.physics.unc.edu/Orca@3331 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Widgets/Plotting/AnalysisSupport/OR1dRoiController.m
@@ -0,0 +1,166 @@
+//
+//  OR1dRoiController.m
+//  
+//
+//  Created by Mark Howe on Tue May 18 2004.
+//  Copyright (c) 2004 CENPA, University of Washington. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+
+#import "OR1dRoiController.h"
+#import "OR1DHistoPlot.h"
+
+@implementation OR1dRoiController
+
++ (id) panel
+{
+    return [[[OR1dRoiController alloc] init]autorelease];
+}
+
+-(id) init
+{
+    if( self = [super init] ){
+        [NSBundle loadNibNamed: @"1dRoi" owner: self];	// We're responsible for releasing the top-level objects in the NIB (our analysisView, right now).
+    }
+    return self;
+}
+
+
+- (void)awakeFromNib
+{	
+	[self updateWindow];
+}
+
+- (void) dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [super dealloc];
+}
+
+- (NSView*) view
+{
+    return analysisView;
+}
+
+- (void) setModel:(id)aModel 
+{
+	model= aModel; 
+    if(model){
+		[self registerNotificationObservers];
+	}
+	else {
+		[[NSNotificationCenter defaultCenter] removeObserver:self];
+		[self updateWindow];
+	}
+}
+
+- (id) model { return model; }
+
+- (void) registerNotificationObservers
+{
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+
+	if(!model)return;
+    
+    NSNotificationCenter* notifyCenter = [NSNotificationCenter defaultCenter];
+        
+    [notifyCenter addObserver : self
+                     selector : @selector(roiMinChanged:)
+                         name : OR1dRoiMinChanged
+                       object : model];
+    
+    [notifyCenter addObserver : self
+                     selector : @selector(roiMaxChanged:)
+                         name : OR1dRoiMaxChanged
+                       object : model];
+    
+    [notifyCenter addObserver : self
+                     selector : @selector(analysisChanged:)
+                         name : OR1dRoiAnalysisChanged
+                       object : model];
+	
+	[notifyCenter addObserver : self
+					 selector : @selector(analysisChanged:)
+						 name : ORRunStatusChangedNotification
+					   object : nil];
+	
+	[self updateWindow];
+
+}
+
+- (void) updateWindow
+{
+    [self roiMinChanged:nil];
+    [self roiMaxChanged:nil];
+    [self analysisChanged:nil];
+}
+
+- (void) roiMinChanged:(NSNotification*)aNotification
+{
+	if(model){
+		[roiMinField setFloatValue: [model minChannel]];
+		[roiWidthField setFloatValue:fabs([model maxChannel]-[model minChannel])];
+	}
+	else {
+		[roiMinField setStringValue:@"---"];	
+		[roiWidthField setStringValue:@"---"];	
+	}
+	
+}
+
+- (void) roiMaxChanged:(NSNotification*)aNotification
+{
+	if(model){
+		[roiMaxField setFloatValue: [model maxChannel]];
+		[roiWidthField setFloatValue:fabs([model maxChannel]-[model minChannel])];
+	}
+	else {          
+		[roiMaxField setStringValue:@"---"];	
+		[roiWidthField setStringValue:@"---"];	
+	}
+
+}
+
+- (void) analysisChanged:(NSNotification*)aNotification
+{
+	if(model){
+		[labelField		setStringValue:	[model label]];
+		[totalSumField	setIntValue:	[model totalSum]];
+		[averageField	setFloatValue:	[model average]];
+		[centroidField	setFloatValue:	[model centroid]];
+		[sigmaField		setFloatValue:	[model sigma]];
+		[roiPeakXField	setIntValue:	(int)[model peakx]];
+		[roiPeakYField	setIntValue:	(int)[model peaky]];
+		if([model useRoiRate]){
+			double theRate = [model roiRate];
+			if(theRate == 0 ) [rateField	setStringValue:@"--"];
+			else if(theRate > 1) [rateField	setStringValue:[NSString stringWithFormat:@"%.2f/sec",theRate]];
+			else				 [rateField	setStringValue:[NSString stringWithFormat:@"%.2E/sec",theRate]];
+		}
+		else [rateField	setStringValue:@"N/A"];
+	}
+	else {
+		[rateField		setStringValue:@"--"];
+		[labelField		setStringValue:@"--"];
+		[totalSumField	setIntValue:0];
+		[averageField	setFloatValue:0];
+		[centroidField	setFloatValue:0];
+		[sigmaField		setFloatValue:0];
+		[roiPeakXField	setIntValue:0];
+		[roiPeakYField	setIntValue:0];
+	}
+}
+
+@end
---
+++ Source/Widgets/Plotting/AnalysisSupport/OR2dRoiController.m
@@ -0,0 +1,110 @@
+//
+//  OR2dRoiController.m
+//  
+//
+//  Created by Mark Howe on Tue May 18 2004.
+//  Copyright (c) 2004 CENPA, University of Washington. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//Washington at the Center for Experimental Nuclear Physics and 
+//Astrophysics (CENPA) sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//Washington reserve all rights in the program. Neither the authors,
+//University of Washington, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+
+#import "OR2dRoiController.h"
+#import "OR1DHistoPlot.h"
+
+@implementation OR2dRoiController
+
++ (id) panel
+{
+    return [[[OR2dRoiController alloc] init]autorelease];
+}
+
+-(id) init
+{
+    if( self = [super init] ){
+        [NSBundle loadNibNamed: @"2dRoi" owner: self];	// We're responsible for releasing the top-level objects in the NIB (our analysisView, right now).
+    }
+    return self;
+}
+
+
+- (void)awakeFromNib
+{	
+	[self updateWindow];
+}
+
+- (void) dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [super dealloc];
+}
+
+- (NSView*) view
+{
+    return analysisView;
+}
+
+- (void) setModel:(id)aModel 
+{
+	model= aModel; 
+    if(model){
+		[self registerNotificationObservers];
+	}
+	else {
+		[[NSNotificationCenter defaultCenter] removeObserver:self];
+		[self updateWindow];
+	}
+}
+
+- (id) model { return model; }
+
+- (void) registerNotificationObservers
+{
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+
+	if(!model)return;
+    
+    NSNotificationCenter* notifyCenter = [NSNotificationCenter defaultCenter];
+    
+    [notifyCenter addObserver : self
+                     selector : @selector(analysisChanged:)
+                         name : OR2dRoiAnalysisChanged
+                       object : model];
+	[self updateWindow];
+
+}
+
+- (void) updateWindow
+{
+    [self analysisChanged:nil];
+}
+
+- (void) analysisChanged:(NSNotification*)aNotification
+{
+	if(model){
+		[labelField		setStringValue:	[model label]];
+		[totalSumField	setIntValue:	[model totalSum]];
+		[averageField	setFloatValue:	[model average]];
+		[peakXField	setIntValue:	(int)[model peakx]];
+		[peakYField	setIntValue:	(int)[model peaky]];
+	}
+	else {
+		[labelField		setStringValue:@"--"];
+		[totalSumField	setIntValue:0];
+		[averageField	setFloatValue:0];
+		[peakXField	setIntValue:0];
+		[peakYField	setIntValue:0];
+	}
+}
+
+@end
