82d39d269d20804e5de74553320f0cd432c4ef5b
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2010-06-12 17:26:27 +0000

    added the encode/decode64 routines
    git-svn-id: svn://orca.physics.unc.edu/Orca@3508 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Cocoa Extensions/NSData+Extensions.h
+++ Source/Cocoa Extensions/NSData+Extensions.h
@@ -30,3 +30,8 @@
 - (NSRect) rectValue;
 - (NSArray *)rowsAndColumns;
 @end
+
+@interface NSData (MBBase64)
++ (id) dataWithBase64EncodedString:(NSString *)string;     //  Padding '=' characters are optional. Whitespace is ignored.
+- (NSString*) base64Encoding;
+@end
