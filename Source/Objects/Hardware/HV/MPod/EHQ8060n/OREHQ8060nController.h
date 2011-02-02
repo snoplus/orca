840dee0b9e55de72f9bd941aefa045cb68ca6fde
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2011-02-02 19:54:22 +0000

    refactored as subclass of iSeg8ChanHV
    git-svn-id: svn://orca.physics.unc.edu/Orca@4462 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Hardware/HV/MPod/EHQ8060n/OREHQ8060nController.h
+++ Source/Objects/Hardware/HV/MPod/EHQ8060n/OREHQ8060nController.h
@@ -18,99 +18,11 @@
 //-------------------------------------------------------------
 
 #pragma mark ***Imported Files
-#import "OrcaObjectController.h"
+#import "ORiSeg8ChanHVController.h"
 
-@class ORPlotView;
-
-@interface OREHQ8060nController : OrcaObjectController 
+@interface OREHQ8060nController : ORiSeg8ChanHVController 
 {
-	IBOutlet NSTextField*	selectedChannelField;
-	IBOutlet NSButton*		shipRecordsButton;
-	IBOutlet NSTextField*	maxCurrentField;
-	IBOutlet NSTextField*	eventField;
-	IBOutlet NSTextField*	riseRateField;
-    IBOutlet NSButton*      settingLockButton;
-    IBOutlet NSButton*      syncButton;
-	IBOutlet NSTextField*	powerField;
-	IBOutlet NSTableView*	hvTableView;
-	IBOutlet NSImageView*	hvStatusImage;
-	//details 
-	IBOutlet NSTextField*	targetField;
-	IBOutlet NSTextField*	voltageField;
-	IBOutlet NSTextField*	stateField;
-    IBOutlet NSButton*      powerOnButton;
-    IBOutlet NSButton*      powerOffButton;
-    IBOutlet NSButton*      stopRampButton;
-    IBOutlet NSButton*      loadButton;
-    IBOutlet NSButton*      rampToZeroButton;
-    IBOutlet NSButton*      panicButton;
-    IBOutlet NSButton*      clearPanicButton;
-	
-	//all channels
-	IBOutlet NSTextField*	channelCountField;
-    IBOutlet NSButton*      powerAllOnButton;
-    IBOutlet NSButton*      powerAllOffButton;
-    IBOutlet NSButton*      stopAllRampButton;
-    IBOutlet NSButton*      loadAllButton;
-    IBOutlet NSButton*      rampAllToZeroButton;
-    IBOutlet NSButton*      panicAllButton;
-    IBOutlet NSButton*      clearAllPanicButton;
-	
-	IBOutlet ORPlotView*   currentPlotter;
-	IBOutlet ORPlotView*   voltagePlotter;
 }
 
 - (id)   init;
-- (void) registerNotificationObservers;
-- (void) updateWindow;
-- (void) updateButtons;
-
-#pragma mark •••Interface Management
-- (void) shipRecordsChanged:(NSNotification*)aNote;
-- (void) maxCurrentChanged:(NSNotification*)aNote;
-- (void) selectedChannelChanged:(NSNotification*)aNote;
-- (void) slotChanged:(NSNotification*)aNote;
-- (void) settingsLockChanged:(NSNotification*)aNote;
-- (void) riseRateChanged:(NSNotification*)aNote;
-- (void) targetChanged:(NSNotification*)aNote;
-- (void) channelReadParamsChanged:(NSNotification*)aNote;
-- (void) powerFailed:(NSNotification*)aNote;
-- (void) powerRestored:(NSNotification*)aNote;
-- (void) updateHistoryPlots:(NSNotification*)aNote;
-- (void) outputStatusChanged:(NSNotification*)aNote;
-
-#pragma mark •••Actions
-- (IBAction) shipRecordsAction:(id)sender;
-- (IBAction) maxCurrentAction:(id)sender;
-- (IBAction) settingLockAction:(id) sender;
-- (IBAction) riseRateAction:(id)sender;
-- (IBAction) targetAction:(id)sender;
-- (IBAction) syncAction:(id)sender;
-- (IBAction) loadAction:(id)sender;
-- (IBAction) powerOnAction:(id)sender;
-- (IBAction) powerOffAction:(id)sender;
-- (IBAction) stopRampAction:(id)sender;
-- (IBAction) rampToZeroAction:(id)sender;
-- (IBAction) panicAction:(id)sender;
-- (IBAction) clearPanicAction:(id)sender;
-- (IBAction) incChannelAction:(id)sender;
-- (IBAction) decChannelAction:(id)sender;
-
-#pragma mark •••Actions for All
-- (IBAction) powerAllOnAction:(id)sender;
-- (IBAction) powerAllOffAction:(id)sender;
-- (IBAction) stopAllRampAction:(id)sender;
-- (IBAction) rampAllToZeroAction:(id)sender;
-- (IBAction) panicAllAction:(id)sender;
-- (IBAction) clearAllPanicAction:(id)sender;
-- (IBAction) loadAllAction:(id)sender;
-
-#pragma mark •••ata Source
-- (int)	numberPointsInPlot:(id)aPlotter;
-- (void) plotter:(id)aPlotter index:(int)i x:(double*)xValue y:(double*)yValue;
-
-@end
-
-@interface NSObject (OREHQ8060nController)
-- (BOOL) power;
 @end
--- Source/Objects/Hardware/HV/MPod/EHQ8060n/OREHQ8060nModel.h
+++ Source/Objects/Hardware/HV/MPod/EHQ8060n/OREHQ8060nModel.h
@@ -19,174 +19,24 @@
 //-------------------------------------------------------------
 
 #pragma mark ***Imported Files
+#import "ORiSeg8ChanHV.h"
 
-#import "ORMPodHVCard.h"
-#import "SBC_Config.h"
-
-@class ORAlarm;
-@class TimedWorker;
-@class ORTimeRate;
-
-#define kNumEHQ8060nChannels   8
-#define kMaxEHQ8060TrendValues 2000
-
-enum {
-	kEHQ8060nOutputOff				 = 0,
-	kEHQ8060nOutputOn				 = 1,
-	kEHQ8060nOutputResetEmergencyOff = 2,
-	kEHQ8060nOutputSetEmergencyOff	 = 3,
-	kEHQ8060nOutputClearEvents		 = 10,
-};
-
-enum {
-	outputOnMask						= (0x1<<0), 
-	outputInhibitMask					= (0x1<<1), 
-	outputFailureMinSenseVoltageMask	= (0x1<<2),
-	outputFailureMaxSenseVoltageMask	= (0x1<<3),
-	
-	outputFailureMaxTerminalVoltageMask = (0x1<<4),
-	outputFailureMaxCurrentMask			= (0x1<<5),
-	outputFailureMaxTemperatureMask		= (0x1<<6),
-	outputFailureMaxPowerMask			= (0x1<<7),
-	
-	outputFailureTimeoutMask			= (0x1<<9),
-	outputCurrentLimitedMask			= (0x1<<10), 
-	outputRampUpMask					= (0x1<<11),
-	outputRampDownMask					= (0x1<<12), 
-	
-	outputEnableKillMask				= (0x1<<13),
-	outputEmergencyOffMask				= (0x1<<14)
-};
-
-#define kEHQ8060nProblemMask (outputFailureMaxTerminalVoltageMask | outputFailureMaxCurrentMask | outputFailureMaxTemperatureMask | outputFailureMaxPowerMask | outputFailureTimeoutMask | outputCurrentLimitedMask)
-
-@interface OREHQ8060nModel : ORMPodHVCard
+@interface OREHQ8060nModel : ORiSeg8ChanHV
 {
-  @private
-	unsigned long   dataId;
-    short			hwGoal[kNumEHQ8060nChannels];		//value to send to hw
-    short			target[kNumEHQ8060nChannels];	//input by user
-    float			riseRate;
-	NSMutableDictionary* rdParams[kNumEHQ8060nChannels];
-    int				selectedChannel;
-    float			maxCurrent[kNumEHQ8060nChannels];
-	
-	ORTimeRate*		voltageHistory[kNumEHQ8060nChannels];
-	ORTimeRate*		currentHistory[kNumEHQ8060nChannels];
-    BOOL			shipRecords;
 }
 
 #pragma mark ***Initialization
-- (id) init;
-- (void) dealloc;
 - (void) setUpImage;
 - (void) makeMainController;
 
 #pragma mark ***Accessors
-- (BOOL)	shipRecords;
-- (void)	setShipRecords:(BOOL)aShipRecords;
-- (float)	maxCurrent:(short)chan;
-- (void)	setMaxCurrent:(short)chan withValue:(float)aMaxCurrent;
-- (int)		selectedChannel;
-- (void)	setSelectedChannel:(int)aSelectedChannel;
-- (int)		slotChannelValue:(int)aChannel;
-- (int)		channel:(int)i readParamAsInt:(NSString*)name;
-- (float)	channel:(int)i readParamAsFloat:(NSString*)name;
-- (id)		channel:(int)i readParamAsObject:(NSString*)name;
-- (id)		channel:(int)i readParamAsValue:(NSString*)name;
-- (float)	riseRate;	
-- (void)	setRiseRate:(float)aValue;
-- (int)		hwGoal:(short)chan;	
-- (void)	setHwGoal:(short)chan withValue:(int)aValue;
-- (int)		target:(short)chan;	
-- (void)	setTarget:(short)chan withValue:(int)aValue;
-- (void)	syncDialog;
-- (void)	commitTargetsToHwGoals;
-- (void)	commitTargetToHwGoal:(int)channel;
-- (NSString*) channelState:(int)channel;
-- (int)		numberChannelsOn;
-- (unsigned long) channelStateMask;
-- (int)		numberChannelsRamping;
-- (int)		numberChannelsWithNonZeroVoltage;
-- (int)		numberChannelsWithNonZeroHwGoal;
-- (unsigned long) failureEvents:(int)channel;
-- (unsigned long) failureEvents;
-
-#pragma mark •••Data Records
-- (unsigned long) dataId;
-- (void) setDataId: (unsigned long) DataId;
-- (void) setDataIds:(id)assigner;
-- (void) syncDataIdsWith:(id)anotherShaper;
-- (NSDictionary*) dataRecordDescription;
-
-#pragma mark ***Polling
-- (void) updateAllValues;
+- (NSString*) settingsLock;
+- (NSString*) name;
 - (NSArray*) channelUpdateList;
 - (NSArray*) commonChannelUpdateList;
-- (NSArray*) addChannelNumbersToParams:(NSArray*)someChannelParams;
-- (NSArray*) addChannel:(int)i toParams:(NSArray*)someChannelParams;
-- (void) processReadResponseArray:(NSArray*)response;
-- (void) processSyncResponseArray:(NSArray*)response;
-- (void) processWriteResponseArray:(NSArray*)response;
 
-#pragma mark •••Hardware Access
-- (void) loadValues:(int)channel;
-- (void) writeVoltage:(int)channel;
-- (void) writeVoltages;
-- (void) writeMaxCurrents;
-- (void) writeMaxCurrent:(int)channel;
-- (void) writeSupervisorBehaviour:(int)channel value:(int)aValue; //test
-- (void) writeRiseTime;
-- (void) writeRiseTime:(float)aValue;
-- (void) turnChannelOn:(int)channel;
-- (void) turnChannelOff:(int)channel;
-- (void) panicChannel:(int)channel;
-- (void) clearPanicChannel:(int)channel;
-- (void) clearEventsChannel:(int)channel;
-- (void) stopRamping:(int)channel;
-- (void) rampToZero:(int)channel;
-- (void) panic:(int)channel;
-
-- (void) loadAllValues;
-- (void) turnAllChannelsOn;
-- (void) turnAllChannelsOff;
-- (void) panicAllChannels;
-- (void) clearAllPanicChannels;
-- (void) clearAllEventsChannels;
-- (void) stopAllRamping;
-- (void) rampAllToZero;
-- (void) panicAll;
-
-#pragma mark •••Trends
-- (ORTimeRate*) voltageHistory:(int)index;
-- (ORTimeRate*) currentHistory:(int)index;
-- (void) shipDataRecords;
-
-#pragma mark •••Convenience Methods
-- (float) voltage:(int)aChannel;
-- (float) current:(int)aChannel;
-
-#pragma mark •••Archival
-- (id)initWithCoder:(NSCoder*)decoder;
-- (void)encodeWithCoder:(NSCoder*)encoder;
-- (NSMutableDictionary*) addParametersToDictionary:(NSMutableDictionary*)dictionary;
-- (void) addCurrentState:(NSMutableDictionary*)dictionary cIntArray:(short*)anArray forKey:(NSString*)aKey;
-- (void) addCurrentState:(NSMutableDictionary*)dictionary cFloatArray:(float*)anArray forKey:(NSString*)aKey;
-- (void) addCurrentState:(NSMutableDictionary*)dictionary cBoolArray:(BOOL*)anArray forKey:(NSString*)aKey;
+#pragma mark •••Hardware Wizard
+- (NSArray*) wizardSelections;
 @end
 
-@interface NSObject (OREHQ8060nModel)
-- (BOOL) power;
-@end
-
-
-extern NSString* OREHQ8060nModelShipRecordsChanged;
-extern NSString* OREHQ8060nModelMaxCurrentChanged;
-extern NSString* OREHQ8060nModelSelectedChannelChanged;
-extern NSString* OREHQ8060nModelRiseRateChanged;
-extern NSString* OREHQ8060nModelHwGoalChanged;
-extern NSString* OREHQ8060nModelTargetChanged;
-extern NSString* OREHQ8060nModelCurrentChanged;
-extern NSString* OREHQ8060nSettingsLock;
-extern NSString* OREHQ8060nModelOutputSwitchChanged;
-extern NSString* OREHQ8060nModelChannelReadParamsChanged;
+extern NSString*  OREHQ8060nSettingsLock;
