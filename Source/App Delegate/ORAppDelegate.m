4fc635e94ddd52137d6ef30310f7aa28cf5f46fb
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2011-09-14 19:51:16 +0000

    15 minute log file snapshot if enabled
    git-svn-id: svn://orca.physics.unc.edu/Orca@5590 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/App Delegate/ORAppDelegate.m
+++ Source/App Delegate/ORAppDelegate.m
@@ -702,7 +702,7 @@ fail:
 			NSString* contents = [NSString stringWithFormat:@"Time:%d\nNext:%d",now,now+kHeartbeatPeriod];
 			[contents writeToFile:finalPath atomically:YES encoding:NSASCIIStringEncoding error:nil];
 		}
-		if(heartbeatCount%10 == 0){
+		if(heartbeatCount%30 == 0){
 			if([[[NSUserDefaults standardUserDefaults] objectForKey:ORPrefPostLogEnabled] intValue]){
 				[[ORStatusController sharedStatusController] doPeriodicSnapShotToPath:[[NSUserDefaults standardUserDefaults] objectForKey:ORPrefHeartBeatPath]];
 			}
