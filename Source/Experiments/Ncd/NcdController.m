7f7fab0314e674fa7de94d72e43786b67ca55fbe
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2011-10-24 13:35:31 +0000

    more Xcode 4 warning fixes
    git-svn-id: svn://orca.physics.unc.edu/Orca@5859 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Experiments/Ncd/NcdController.m
+++ Source/Experiments/Ncd/NcdController.m
@@ -469,6 +469,7 @@
     }
 #if defined(MAC_OS_X_VERSION_10_6) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6 // 10.6-specific
     [savePanel setDirectoryURL:[NSURL URLWithString:startingDir]];
+    [savePanel setNameFieldStringValue:defaultFile];
     [savePanel beginSheetModalForWindow:[self window] completionHandler:^(NSInteger result){
         if (result == NSFileHandlingPanelOKButton) {
             [[NcdDetector sharedInstance] saveMapFileAs:[[savePanel URL]path]];
