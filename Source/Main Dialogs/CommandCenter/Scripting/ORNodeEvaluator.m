3107dfa24d98110ea4592997aeecb8e36f2e3f29
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2011-11-22 17:03:48 +0000

    fixed a special case of a global compare to a number
    git-svn-id: svn://orca.physics.unc.edu/Orca@6024 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Main Dialogs/CommandCenter/Scripting/ORNodeEvaluator.m
+++ Source/Main Dialogs/CommandCenter/Scripting/ORNodeEvaluator.m
@@ -1360,17 +1360,17 @@
 {
 	if(([a class] == [b class]) && ([a class] == [NSDecimalNumber class]) && ([b class] == [NSDecimalNumber class])) return [a compare:b];
 	else if(([a class] != [NSDecimalNumber class]) && ([b class] == [NSDecimalNumber class])){
-		return [[NSDecimalNumber numberWithLong:(unsigned long)a] compare:b];
+		return [[NSDecimalNumber decimalNumberWithString:a] compare:b];
 	}
 	else if(([b class] != [NSDecimalNumber class]) && ([a class] == [NSDecimalNumber class])){
-		return [[NSDecimalNumber numberWithLong:(unsigned long)b] compare:a];
+		return [a compare:[NSDecimalNumber decimalNumberWithString:b]];
 	}
 	else if([[a className] isEqualToString:@"NSCFString"] && [[b className] isEqualToString:@"NSCFString"]){
 		if([a isEqualToString:b])return NSOrderedSame;
 		else return NSOrderedDescending;
 	}
 	else if([a class] != [NSDecimalNumber class] && [b class] != [NSDecimalNumber class]){
-		return [[NSDecimalNumber numberWithLong:(unsigned long)a] compare:[NSDecimalNumber numberWithLong:(unsigned long)b]];
+		return [[NSDecimalNumber decimalNumberWithString:a] compare:[NSDecimalNumber decimalNumberWithString:b]];
 	}
 	else return NSOrderedDescending;
 	[NSException raise:@"Run time" format:@"illegal Args to compare"];
