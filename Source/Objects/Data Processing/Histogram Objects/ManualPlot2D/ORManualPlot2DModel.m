b2ac8626497d2ccff0c7820d37210d0f60c6e5dd
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2012-03-23 17:40:15 +0000

    new
    git-svn-id: svn://orca.physics.unc.edu/Orca@6646 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Objects/Data Processing/Histogram Objects/ManualPlot2D/ORManualPlot2DModel.m
@@ -0,0 +1,251 @@
+//
+//  ORManualPlot2DModel.m
+//  Orca
+//
+//  Created by Mark Howe on Fri Mar 23,2012.
+//  Copyright (c) 2012  University of North Carolina. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//North Carolina  sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//North Carolina reserve all rights in the program. Neither the authors,
+//University of North Carolina, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+#pragma mark •••Imported Files
+#import "ORManualPlot2DModel.h"
+#import "OR2dRoi.h"
+
+NSString* ORManualPlot2DLock					= @"ORManualPlot2DLock";
+NSString* ORManualPlot2DDataChanged				= @"ORManualPlot2DDataChanged";
+NSString* ORManualPlot2DModelXTitleChanged		= @"ORManualPlot2DModelXTitleChanged";
+NSString* ORManualPlot2DModelYTitleChanged		= @"ORManualPlot2DModelYTitleChanged";
+NSString* ORManualPlot2DModelPlotTitleChanged	= @"ORManualPlot2DModelPlotTitleChanged";
+
+@implementation ORManualPlot2DModel
+
+#pragma mark •••initialization
+- (id) init 
+{
+    self = [super init];
+	dataSetLock = [[NSLock alloc] init];
+    histogram = nil;
+    return self;    
+}
+
+- (void) dealloc
+{
+    [self freeHistogram];
+    [xTitle release];
+    [yTitle release];
+    [plotTitle release];
+	[dataSetLock release];
+    [NSObject cancelPreviousPerformRequestsWithTarget:self];
+	[rois release];
+    [super dealloc];
+}
+
+- (NSString*) fullName
+{
+	return [self fullID];
+}
+- (NSString*) fullNameWithRunNumber
+{
+	//fake out so we can inherit
+	return [self fullID];
+}
+
+- (void) freeHistogram
+{
+    if(histogram) {
+        free(histogram);
+        histogram = 0;
+    }
+}
+
+- (void) clear
+{
+	[dataSetLock lock];
+    if(histogram){
+        memset(histogram,0,numberBinsPerSide*numberBinsPerSide*sizeof(long));
+    }
+    maxX = numberBinsPerSide-1;
+    minX = 0;
+    maxY = numberBinsPerSide-1;
+    minY = 0;
+	[dataSetLock unlock];
+}
+
+- (unsigned short) numberBinsPerSide
+{
+    return numberBinsPerSide;
+}
+
+- (void) setNumberBinsPerSide:(unsigned short)bins
+{
+	if(bins>256){
+		NSLog(@"Sorry -- number of bins in the manual 2D plot is limited to 256x256\n");
+		bins=256;
+	}
+	[dataSetLock lock];
+    numberBinsPerSide = bins;
+    [self freeHistogram];
+    histogram = (unsigned long*)malloc(numberBinsPerSide*numberBinsPerSide*sizeof(unsigned long));
+	[dataSetLock unlock];
+    [self clear];
+}
+
+- (unsigned long)valueAtX:(unsigned short)aXBin y:(unsigned short)aYBin
+{
+	unsigned long theResult = 0;
+	if(aXBin<numberBinsPerSide && aYBin<numberBinsPerSide){
+		[dataSetLock lock];
+		aXBin = aXBin % numberBinsPerSide;   // Error Check Our x Value
+		aYBin = aYBin % numberBinsPerSide;   // Error Check Our y Value
+		theResult =  histogram[aXBin + aYBin*numberBinsPerSide];
+		[dataSetLock unlock];
+	}
+	return theResult;
+}
+
+- (void) setBinAtX:(int)aXBin y:(int)aYBin to:(unsigned long)aValue;
+{
+	if(histogram && aXBin<numberBinsPerSide && aYBin<numberBinsPerSide){
+		[dataSetLock lock];	
+		histogram[aXBin + aYBin*numberBinsPerSide] = aValue;
+		[dataSetLock unlock];	
+		[[NSNotificationCenter defaultCenter] postNotificationOnMainThreadWithName:ORManualPlot2DDataChanged object:self];
+	}
+}
+
+- (void) incrementBinAtX:(int)aXBin y:(int)aYBin by:(unsigned long)incValue;
+{
+	if(histogram && aXBin<numberBinsPerSide && aYBin<numberBinsPerSide){
+		[dataSetLock lock];	
+		histogram[aXBin + aYBin*numberBinsPerSide]+=incValue;
+		[dataSetLock unlock];	
+		[[NSNotificationCenter defaultCenter] postNotificationOnMainThreadWithName:ORManualPlot2DDataChanged object:self];
+	}
+}
+
+#pragma mark ***Accessors
+- (void) postUpdate
+{
+	[[NSNotificationCenter defaultCenter] postNotificationName:ORManualPlot2DDataChanged object:self];    
+}
+
+- (NSString*) xTitle
+{
+    if(xTitle)return xTitle;
+	else return @"x";
+}
+
+- (void) setXTitle:(NSString*)aString
+{
+    [xTitle autorelease];
+    xTitle = [aString copy];    
+	
+    [[NSNotificationCenter defaultCenter] postNotificationName:ORManualPlot2DModelXTitleChanged object:self];
+}
+
+- (NSString*) yTitle
+{
+    if(yTitle)return yTitle;
+	else return @"y";
+}
+
+- (void) setYTitle:(NSString*)aString
+{
+    [yTitle autorelease];
+    yTitle = [aString copy];    
+
+    [[NSNotificationCenter defaultCenter] postNotificationName:ORManualPlot2DModelYTitleChanged object:self];
+}
+
+- (NSString*) plotTitle
+{
+	if(plotTitle) return plotTitle;
+	else return @"2D Manual Plot";
+}
+
+- (void) setPlotTitle:(NSString*)aString
+{
+    [plotTitle autorelease];
+    plotTitle = [aString copy];    
+
+    [[NSNotificationCenter defaultCenter] postNotificationName:ORManualPlot2DModelPlotTitleChanged object:self];
+}
+
+- (void) setUpImage
+{
+	[self setImage:[NSImage imageNamed:@"ManualPlot2D"]];
+}
+
+- (void) makeMainController
+{
+    [self linkToController:@"ORManualPlot2DController"];
+}
+
+- (NSString*) helpURL
+{
+	return @"Subsystems/Manual_2DPlotter.html";
+}
+
+#pragma mark •••Archival
+- (id)initWithCoder:(NSCoder*)decoder
+{
+    self = [super initWithCoder:decoder];
+	
+    [[self undoManager] disableUndoRegistration];
+ 	rois = [[decoder decodeObjectForKey:@"rois"] retain];
+    [[self undoManager] enableUndoRegistration];
+
+	dataSetLock = [[NSLock alloc] init];
+	
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder*)encoder
+{
+    [super encodeWithCoder:encoder];
+    [encoder encodeObject:rois forKey:@"rois"];
+}
+
+#pragma mark •••Data Source
+- (NSMutableArray*) rois
+{
+	if(!rois){
+		rois = [[NSMutableArray alloc] init];
+		[rois addObject:[[[OR2dRoi alloc] initAtPoint:NSMakePoint(50,50)] autorelease]];
+	}
+	return rois;
+}
+- (unsigned long*) plotter:(id)aPlotter numberBinsPerSide:(unsigned short*)xValue
+{
+    return [self getDataSetAndNumBinsPerSize:xValue];
+}
+
+- (void) plotter:(id)aPlotter xMin:(unsigned short*)aMinX xMax:(unsigned short*)aMaxX yMin:(unsigned short*)aMinY yMax:(unsigned short*)aMaxY
+{
+    [self getXMin:aMinX xMax:aMaxX yMin:aMinY yMax:aMaxY];
+}
+
+- (unsigned long*) getDataSetAndNumBinsPerSize:(unsigned short*)value
+{
+    *value = numberBinsPerSide;
+    return histogram;
+    
+}
+
+- (void) getXMin:(unsigned short*)aMinX xMax:(unsigned short*)aMaxX yMin:(unsigned short*)aMinY yMax:(unsigned short*)aMaxY
+{
+    *aMinX = minX;
+    *aMaxX = maxX;
+    *aMinY = minY;
+    *aMaxY = maxY;
+}
+@end
