24a4c5f4232931d5a7f1e5d905e006eeb9389d93
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2012-06-08 20:27:20 +0000

    new
    git-svn-id: svn://orca.physics.unc.edu/Orca@6993 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Objects/Hardware/Vme/CAEN/CV830/ORCV830Decoders.m
@@ -0,0 +1,167 @@
+//
+//  ORCV830Decoders.m
+//  Orca
+//
+//  Created by Mark Howe on 06/06/2012
+// Copyright (c) 2012 University of North Carolina. All rights reserved.
+//
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//North Carolina sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//North Carolina reserve all rights in the program. Neither the authors,
+//University of North Carolina,or U.S. Government make any warranty,
+//express or implied,or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+
+#import "ORCV830Decoders.h"
+#import "ORDataPacket.h"
+#import "ORDataSet.h"
+#import "ORCV830Model.h"
+#import "ORDataTypeAssigner.h"
+
+/* Event Record
+xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+^^^^ ^^^^ ^^^^ ^^----------------------- V830 ID (from header)
+-----------------^^ ^^^^ ^^^^ ^^^^ ^^^^- length (variable)
+xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+--------^-^^^--------------------------- Crate number
+-------------^-^^^^--------------------- Card number
+xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+-------------------------------------^^- Acq Mode. 0=disabled,1=Random, 2=periodic
+------------------------------------^---- Data Format 0=32Bit,1=24Bit
+-----------------------------------^----- Header Enabled
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx- Enabled Mask
+xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx  counter 0 or header
+..
+..
+xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx  counter 31 //note that only enabled channels are included so list may be shorter
+*/
+
+
+@implementation ORCV830DecoderForEvent
+
+- (unsigned long) decodeData:(void*)someData fromDecoder:(ORDecoder*)aDecoder intoDataSet:(ORDataSet*)aDataSet
+{
+    unsigned long* ptr		= (unsigned long*)someData;
+	unsigned long length	= ExtractLength(ptr[0]);
+	int crate				= ShiftAndExtract(ptr[1],21,0xf);
+	int card				= ShiftAndExtract(ptr[1],16,0x1f);
+	unsigned long enabledMask	= ptr[3];
+
+	NSString* crateKey   = [self getCrateKey: crate];
+	NSString* cardKey    = [self getCardKey: card];
+	
+	ptr+=5;					//point to first word of data (past header)
+	
+	int i;
+	for(i=0;i<32;i++){
+		if(enabledMask & (0x1L<<i)){
+			NSString* valueString = [NSString stringWithFormat:@"%u",*ptr];
+			NSString* channelKey = [self getChannelKey:i];
+
+			[aDataSet loadGenericData:valueString sender:self withKeys:@"Scaler Event",@"V830",  crateKey,cardKey,channelKey,nil];
+			ptr++;
+		}
+	}
+    return length; //must return number of longs processed.
+}
+
+- (NSString*) dataRecordDescription:(unsigned long*)ptr
+{
+    NSString* title= @"CV830 Scaler Record\n\n";
+	int crateNum			= ShiftAndExtract(ptr[1],21,0xf);
+	int cardNum				= ShiftAndExtract(ptr[1],16,0x1f);
+	int dataIs24Bit			= ShiftAndExtract(ptr[2],2,0x1);
+	int enabledMask			= ptr[3];
+
+    NSString* crate = [NSString stringWithFormat:@"Crate = %d\n",crateNum];
+    NSString* card  = [NSString stringWithFormat:@"Card  = %d\n",cardNum];
+	NSString* s = [NSString stringWithFormat:@"%@%@%@\n",title,crate,card];
+	s = [s stringByAppendingFormat:@"Enabled Mask:0X%08X\n",enabledMask];
+	
+	ptr+=5;					//point to first word of data (past header)
+	
+	int i;
+	for(i=0;i<32;i++){
+		if(enabledMask & (0x1L<<i)){
+			if(dataIs24Bit) s = [s stringByAppendingFormat:@"Channel:%d Counter: %u\n",i,*ptr*0xFFFFFF];
+			else			s = [s stringByAppendingFormat:@"Channel:%d Counter: %u\n",i,*ptr];
+			ptr++;
+		}
+	}
+
+    return s;               
+}
+
+
+@end
+
+/* Polled Scalers Record
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+ ^^^^ ^^^^ ^^^^ ^^----------------------- V830 ID (from header)
+ -----------------^^ ^^^^ ^^^^ ^^^^ ^^^^- length (always 20 longs)
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+ --------^-^^^--------------------------- Crate number
+ -------------^-^^^^--------------------- Card number
+ --------------------^^^^ ^^^^ ^^^^ ^^^^- enabled mask
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx  UT time of last read
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx  counter 0
+ ..
+ ..
+ xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx  counter 31
+ */
+
+
+@implementation ORCV830DecoderForPolledRead
+
+- (unsigned long) decodeData:(void*)someData fromDecoder:(ORDecoder*)aDecoder intoDataSet:(ORDataSet*)aDataSet
+{
+    unsigned long* ptr = (unsigned long*)someData;
+	unsigned long length	= ExtractLength(ptr[0]);
+	int crateNum	= ShiftAndExtract(ptr[1],21,0xf);
+	int cardNum		= ShiftAndExtract(ptr[1],16,0x1f);
+	unsigned long enabledMask = ptr[2];
+	NSString* crateKey = [self getCrateKey: crateNum];
+	NSString* cardKey = [self getCardKey: cardNum];
+	int i;
+	for(i=0;i<kNumCV830Channels;i++){
+		if(enabledMask & (0x1L<<i)){
+			NSString* valueString = [NSString stringWithFormat:@"%u",ptr[4+i]];
+			[aDataSet loadGenericData:valueString sender:self withKeys:@"Scalers",@"V830",  crateKey,cardKey,[self getChannelKey:i],nil];
+		}
+	}
+    return length; //must return number of longs processed.
+}
+
+- (NSString*) dataRecordDescription:(unsigned long*)ptr
+{
+    NSString* title= @"CV830 Scaler Record\n\n";
+	int crateNum	= ShiftAndExtract(ptr[1],21,0xf);
+	int cardNum		= ShiftAndExtract(ptr[1],16,0x1f);
+	unsigned long enabledMask = ptr[2];
+    NSString* crate = [NSString stringWithFormat:@"Crate = %d\n",crateNum];
+    NSString* card  = [NSString stringWithFormat:@"Card  = %d\n",(*ptr&0x001f0000)>>16];
+    NSString* mask  = [NSString stringWithFormat:@"Mask  = 0x%x\n",cardNum];
+	NSCalendarDate* date = [NSCalendarDate dateWithTimeIntervalSince1970:ptr[3]];
+	[date setCalendarFormat:@"%m/%d/%y %H:%M:%S %z\n"];
+	int i;
+	NSString* s = [NSString stringWithFormat:@"%@%@%@%@%@",title,crate,card,mask,date];
+	for(i=0;i<kNumCV830Channels;i++){
+		if(enabledMask & (0x1L<<i)){
+			s = [s stringByAppendingFormat:@"%d:%u\n",i,ptr[4+i]];
+		}
+	}
+	
+    return s;               
+}
+
+
+@end
+
+
---
+++ Source/Objects/Hardware/Vme/CAEN/CV830/ORCV830Model.h
@@ -0,0 +1,212 @@
+/*
+ *  ORCV830Model.h
+ *  Orca
+ *
+ *  Created by Mark Howe on 06/06/2012
+ *  Copyright (c) 2012 University of North Carolina. All rights reserved.
+ *
+ */
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//North Carolina sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//North Carolina reserve all rights in the program. Neither the authors,
+//University of North Carolina,or U.S. Government make any warranty,
+//express or implied,or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+#pragma mark •••Imported Files
+
+#import "ORCaenCardModel.h"
+#import "VME_eCPU_Config.h"
+#import "SBC_Config.h"
+#import "ORDataTaker.h"
+
+#define 	kNumCV830Channels 	32
+
+#pragma mark •••Register Definitions
+enum {
+	kEventBuffer,		 
+	kCounter0,
+	kCounter1,
+	kCounter2,
+	kCounter3,
+	kCounter4,
+	kCounter5,
+	kCounter6,
+	kCounter7,
+	kCounter8,
+	kCounter9,
+	kCounter10,
+	kCounter11,
+	kCounter12,
+	kCounter13,
+	kCounter14,
+	kCounter15,
+	kCounter16,
+	kCounter17,
+	kCounter18,
+	kCounter19,
+	kCounter20,
+	kCounter21,
+	kCounter22,
+	kCounter23,
+	kCounter24,	
+	kCounter25,
+	kCounter26,
+	kCounter27,
+	kCounter28,
+	kCounter29,
+	kCounter30,
+	kCounter31,
+	kTestReg,
+	kTestlcntl,
+	kTestlcnth,
+	kTesthcntl,
+	kTesthcnth,
+	kChannelEnable,
+	kDwellTime,
+	kControlReg,
+	kBitSetReg,
+	kBitClrReg,
+	kStatusReg,
+	kGEOReg,
+	kInterruptLevel,
+	kInterruptVector,
+	kADER32,
+	kADER23,
+	kEnableADER,
+	kMCSTBaseAdd,
+	kMCSTControl,
+	kModuleReset,
+	kSoftwareClear,
+	kSoftwareTrig,
+	kTrigCounter,
+	kAlmostFull,
+	kBLTEventNum,
+	kFirmware,
+	kMEBEventNum,
+	kDummy32,
+	kDummy16,
+	kConfigROM,
+	kNumberOfV830Registers			//must be last
+};
+
+#pragma mark •••Forward Declarations
+@class ORRateGroup;
+@class ORReadOutList;
+
+@interface ORCV830Model :  ORCaenCardModel <ORDataTaker>
+{
+    @private
+		BOOL			pollRunning;
+        unsigned long	enabledMask;
+		unsigned long	scalerValue[kNumCV830Channels];
+		NSTimeInterval	pollingState;
+		BOOL			shipRecords;
+		time_t			lastReadTime;
+		BOOL			isRunning;
+		BOOL			scheduledForUpdate;
+		unsigned long	dwellTime;
+		short			acqMode;
+		BOOL			testMode;
+		BOOL			dataFormat;
+		BOOL			clearMeb;
+		BOOL			autoReset;
+		unsigned long	polledDataId;
+		unsigned long	dataRecord[38];
+		ORReadOutList*	readOutGroup;
+		//some cached variabled
+		NSArray* dataTakers;	//cache of data takers.
+		unsigned long numEnabledChannels;
+}
+
+#pragma mark •••Initialization
+- (id) init; 
+- (void) dealloc;
+- (void) setUpImage;
+- (void) makeMainController;
+
+#pragma mark •••Accessors
+- (BOOL) autoReset;
+- (void) setAutoReset:(BOOL)aAutoReset;
+- (BOOL) clearMeb;
+- (void) setClearMeb:(BOOL)aClearMeb;
+- (BOOL) dataFormat;
+- (void) setDataFormat:(BOOL)aDataFormat;
+- (BOOL) testMode;
+- (void) setTestMode:(BOOL)aTestMode;
+- (short) acqMode;
+- (void) setAcqMode:(short)aAcqMode;
+- (unsigned long) dwellTime;
+- (void) setDwellTime:(unsigned long)aDwellTime;
+- (BOOL) shipRecords;
+- (void) setShipRecords:(BOOL)aShipRecords;
+- (unsigned long) scalerValue:(int)index;
+- (void) setScalerValue:(unsigned long)aValue index:(int)index;
+- (unsigned long) enabledMask;
+- (void) setEnabledMask:(unsigned long)aEnabledMask;
+- (void) setPollingState:(NSTimeInterval)aState;
+- (NSTimeInterval) pollingState;
+- (void) registerNotificationObservers;
+- (void) runAboutToStop:(NSNotification*)aNote;
+
+#pragma mark •••Hardware Access
+- (void) initBoard;
+- (void) readStatus;
+- (void) readScalers;
+- (void) writeEnabledMask;
+- (void) softwareTrigger;
+- (void) softwareClear;
+- (void) softwareReset;
+- (void) writeControlReg;
+- (void) writeDwellTime;
+- (unsigned short) getNumEvents;
+- (unsigned short) numEnabledChannels;
+
+#pragma mark •••Data Header
+- (unsigned long) polledDataId;
+- (void) setPolledDataId: (unsigned long) DataId;
+- (NSDictionary*) dataRecordDescription;
+
+#pragma mark ***DataTaker
+- (NSMutableDictionary*) addParametersToDictionary:(NSMutableDictionary*)dictionary;
+- (int)  load_HW_Config_Structure:(SBC_crate_config*)configStruct index:(int)index;
+- (void) setDataIds:(id)assigner;
+- (void) syncDataIdsWith:(id)anotherObj;
+- (unsigned long) dataId;
+- (void) setDataId: (unsigned long) DataId;
+- (void) reset;
+- (void) runTaskStarted:(ORDataPacket*) aDataPacket userInfo:(id)userInfo;
+- (void) takeData:(ORDataPacket*)aDataPacket userInfo:(id)userInfo;
+- (void) runTaskStopped:(ORDataPacket*) aDataPacket userInfo:(id)userInfo;
+
+#pragma mark ***Children
+- (ORReadOutList*)	readOutGroup;
+- (void)			setReadOutGroup:(ORReadOutList*)newReadOutGroup;
+- (NSMutableArray*) children;
+- (void) saveReadOutList:(NSFileHandle*)aFile;
+- (void) loadReadOutList:(NSFileHandle*)aFile;
+
+#pragma mark •••Archival
+- (id) initWithCoder:(NSCoder*)decoder;
+- (void) encodeWithCoder:(NSCoder*)encoder;
+@end
+
+#pragma mark •••External String Definitions
+extern NSString* ORCV830ModelAutoResetChanged;
+extern NSString* ORCV830ModelClearMebChanged;
+extern NSString* ORCV830ModelDataFormatChanged;
+extern NSString* ORCV830ModelTestModeChanged;
+extern NSString* ORCV830ModelAcqModeChanged;
+extern NSString* ORCV830ModelDwellTimeChanged;
+extern NSString* ORCV830ModelEnabledMaskChanged;
+extern NSString* ORCV830ModelScalerValueChanged;
+extern NSString* ORCV830ModelPollingStateChanged;
+extern NSString* ORCV830ModelShipRecordsChanged;
+extern NSString* ORCV830ModelAllScalerValuesChanged;
+
