d568a379dc5fb14b62c1aad7d4fabfd18463bd1f
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2012-09-17 22:42:41 +0000

    the calibration class is continuing to corrupt the config files sometimes. Put in a class check to help prevent the problem until the true cause is found.
    git-svn-id: svn://orca.physics.unc.edu/Orca@7419 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Misc Objects/Views/Calibration/ORCalibration.m
+++ Source/Objects/Misc Objects/Views/Calibration/ORCalibration.m
@@ -416,30 +416,11 @@
 - (id)initWithCoder:(NSCoder*)decoder
 {
     self		= [super init];
-    calibrationArray =			[[decoder decodeObjectForKey:	@"calibrationArray"] retain];
-	//check if we need to be backward compatible with the old form which used just an array
-	if([calibrationArray count]){
-		if(![[calibrationArray objectAtIndex:0] isKindOfClass:NSClassFromString(@"NSMutableDictionary")]){
-			int i;
-			int n = [calibrationArray count];
-			NSMutableArray* newArray = [NSMutableArray array];
-			if(n%2 == 0){
-				for(i=0;i<n;i+=2){
-					id chan = [calibrationArray objectAtIndex:i];
-					id energy = [calibrationArray objectAtIndex:i+1];
-					[newArray addObject:[NSMutableDictionary dictionaryWithObject:chan forKey:@"Channel"]];
-					[newArray addObject:[NSMutableDictionary dictionaryWithObject:energy forKey:@"Energy"]];
-				}
-				[calibrationArray release];
-				calibrationArray = [newArray retain];
-			}
-			else {
-				[calibrationArray release];
-				calibrationArray = nil;
-			}
-		}
-	}
-	
+    calibrationArray =			[[decoder decodeObjectForKey:@"calibrationArray"] retain];
+    if(![calibrationArray isKindOfClass:NSClassFromString(@"NSArray")]){
+        [calibrationArray release];
+        calibrationArray = nil;
+    }
 	[self setUnits:				[decoder decodeObjectForKey:@"units"]];
 	[self setLabel:				[decoder decodeObjectForKey:@"label"]];
 	[self setIgnoreCalibration:	[decoder decodeBoolForKey:@"ignoreCalibration"]];
@@ -451,7 +432,11 @@
 
 - (void)encodeWithCoder:(NSCoder*)encoder
 {
-    [encoder encodeObject:calibrationArray	forKey: @"calibrationArray"];
+    //it appears that sometimes the calibration array can be something other than an array
+    //if the cause is ever found the class check can be removed.
+    if([calibrationArray isKindOfClass:NSClassFromString(@"NSArray")]){
+        [encoder encodeObject:calibrationArray	forKey: @"calibrationArray"];
+    }
 	[encoder encodeObject:units				forKey:@"units"];
 	[encoder encodeObject:label				forKey:@"label"];
 	[encoder encodeBool:ignoreCalibration	forKey:@"ignoreCalibration"];
