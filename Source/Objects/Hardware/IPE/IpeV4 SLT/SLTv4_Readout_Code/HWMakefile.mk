e6d6a954838c2a5d7ba8e2ae8e83d3e839fa55c5
bergmann <bergmann@fd86f90f-2c0a-422a-b972-84106d8db31b>
2013-04-26 17:46:20 +0000

    IPE V4 KATRIN DAQ: automatic SLT DMA driver usage (compiler options for SBC code). -tb-
    git-svn-id: svn://orca.physics.unc.edu/Orca@8240 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Hardware/IPE/IpeV4 SLT/SLTv4_Readout_Code/HWMakefile.mk
+++ Source/Objects/Hardware/IPE/IpeV4 SLT/SLTv4_Readout_Code/HWMakefile.mk
@@ -3,6 +3,16 @@
 -include linkwithdmalib.mk
 
 
+# 2013-04 changes -tb-
+# automatic compilation with PCIDMA on 64 bit machines or when SLT driver ver. 3 is loaded.
+#SLTDRVVERSION=$(shell cat /proc/devices | awk '/fzk_ipe_slt/{print $2}'  | cut -b 12-)
+SLTDRVVERSION=$(shell cat /proc/devices | grep fzk_ipe_slt   | tail -c2)
+#testflags = xxx $(cat /proc/devices | grep fzk_ipe_slt | awk '/fzk_ipe_slt/{print $2}'  | cut -b 12-) yyy
+#testflags = xxx $(shell cat /proc/devices | grep fzk_ipe_slt   | tail -c2) yyy
+#sltdrvversion = -DSLTDRVVERSION=$(shell cat /proc/devices | grep fzk_ipe_slt   | tail -c2)
+sltdrvversion = -DSLTDRVVERSION=$(SLTDRVVERSION)
+
+
 # 2013-03 changes -tb-
 # Simulation mode and new Macs (as local host): they use clang, not gcc any more:
 # - removed compile option -gstabs+ (GNU GCC specific)
@@ -44,13 +54,18 @@ else
 endif
 
 #-tb- Conditional compiling (with pci dma library or with standard pci lib):
+# added 2013-04: if DPMC_LINK_WITH_DMA_LIB is not set explicitly,
+#                compilation is done by checking the host system
 ifeq ($(PMC_LINK_WITH_DMA_LIB),1)
   defflags2='-DPMC_LINK_WITH_DMA_LIB=1'
-else
+endif
+ifeq ($(PMC_LINK_WITH_DMA_LIB),0)
   defflags2='-DPMC_LINK_WITH_DMA_LIB=0'
 endif
 
-UCFLAGS =  -g -Wall   -I ~/src/v4/fdhwlib/src  $(defflags)  $(defflags2)
+
+
+UCFLAGS =  -g -Wall   -I ~/src/v4/fdhwlib/src  $(defflags)  $(defflags2) $(sltdrvversion)
 #2013-03: removed  -gstabs+: UCFLAGS =  -g -Wall  -gstabs+ -I ~/src/v4/fdhwlib/src  $(defflags)  $(defflags2)
 #Note: -Wno-sign-compare supresses 'comparison int with unsigned int' compiler warning -tb-
 #LFLAGS  = -fexceptions -lpbusaccess -lPbus1394 -lakutil -lpthread -lstdc++ \
@@ -69,7 +84,7 @@ UCFLAGS =  -g -Wall   -I ~/src/v4/fdhwlib/src  $(defflags)  $(defflags2)
 #LFLAGS  =  -fexceptions -lPbusPCI  -lkatrinhw4 -lhw4 -lakutil -lpthread -lstdc++ 
 #LIBs    = 
 
-
+#SYSTEMARCH = x86_64
 #-tb- Conditional compiling (with hardware library or with hardware simulation):
 #without DMA:  LFLAGS  =  -fexceptions -lPbusPCI  -lkatrinhw4 -lhw4 -lakutil -lpthread -lstdc++
 #for DMA:    LFLAGS  =  -fexceptions -lPbusPCIDMA  -lkatrinhw4 -lhw4 -lakutil -lpthread -lstdc++
@@ -78,9 +93,25 @@ ifeq ($(PMC_COMPILE_IN_SIMULATION_MODE),1)
 else
   ifeq ($(PMC_LINK_WITH_DMA_LIB),1)
     LFLAGS  =  -fexceptions -lPbusPCIDMA  -lkatrinhw4 -lhw4 -lakutil -lpthread -lstdc++
-  else
+  else ifeq ($(PMC_LINK_WITH_DMA_LIB),0)
     LFLAGS  =  -fexceptions -lPbusPCI  -lkatrinhw4 -lhw4 -lakutil -lpthread -lstdc++
+  else
+      #2013-04 if not explicitly defined, we try to detect the architecture (32/64 bit machine) and SLT driver version -tb-
+      #SYSTEMARCH = $(shell uname -m)
+      #ifeq ($(SYSTEMARCH),x86_64)
+      ifeq ($(SLTDRVVERSION),3)
+          #TODO - TODO - TODO - TODO - TODO - TODO - TODO - for new driver versions I need to extend this! currently only ver.3 -tb-
+          PMC_LINK_WITH_DMA_LIB=1
+          defflags2='-DPMC_LINK_WITH_DMA_LIB=1'
+          LFLAGS  =  -fexceptions -lPbusPCIDMA  -lkatrinhw4 -lhw4 -lakutil -lpthread -lstdc++
+      else
+          PMC_LINK_WITH_DMA_LIB=0
+          defflags2='-DPMC_LINK_WITH_DMA_LIB=0'
+          LFLAGS  =  -fexceptions -lPbusPCI  -lkatrinhw4 -lhw4 -lakutil -lpthread -lstdc++
+      endif
   endif
+
+
 
 endif
 
