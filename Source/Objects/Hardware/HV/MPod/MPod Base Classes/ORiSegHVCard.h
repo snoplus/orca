cfca18e7915aec7f536d260b035903370264cad9
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2013-05-21 13:26:59 +0000

    name change and refactored
    git-svn-id: svn://orca.physics.unc.edu/Orca@8301 fd86f90f-2c0a-422a-b972-84106d8db31b

---
+++ Source/Objects/Hardware/HV/MPod/MPod Base Classes/ORiSegHVCard.h
@@ -0,0 +1,201 @@
+//
+//  ORMPodiSegHVCardCard.h
+//  Orca
+//
+//  Created by Mark Howe on Wed Feb 2,2011
+//  Copyright (c) 2011 University of North Carolina. All rights reserved.
+//-----------------------------------------------------------
+//This program was prepared for the Regents of the University of 
+//North Carolina Department of Physics and Astrophysics 
+//sponsored in part by the United States 
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
+//The University has certain rights in the program pursuant to 
+//the contract and the program should not be copied or distributed 
+//outside your organization.  The DOE and the University of 
+//North Carolina reserve all rights in the program. Neither the authors,
+//University of North Carolina, or U.S. Government make any warranty, 
+//express or implied, or assume any liability or responsibility 
+//for the use of this software.
+//-------------------------------------------------------------
+
+#pragma mark ¥¥¥Imported Files
+#import "ORMPodHVCard.h"
+#import "ORHWWizard.h"
+
+@class ORTimeRate;
+
+#define kPositivePolarity 1
+#define kNegativePolarity 0
+
+enum {
+	kiSegHVCardOutputOff                    = 0,
+	kiSegHVCardOutputOn                     = 1,
+	kiSegHVCardOutputResetEmergencyOff      = 2,
+	kiSegHVCardOutputSetEmergencyOff        = 3,
+	kiSegHVCardOutputClearEvents            = 10,
+};
+
+enum {
+	outputOnMask						= (0x1<<0), 
+	outputInhibitMask					= (0x1<<1), 
+	outputFailureMinSenseVoltageMask	= (0x1<<2),
+	outputFailureMaxSenseVoltageMask	= (0x1<<3),
+	
+	outputFailureMaxTerminalVoltageMask = (0x1<<4),
+	outputFailureMaxCurrentMask			= (0x1<<5),
+	outputFailureMaxTemperatureMask		= (0x1<<6),
+	outputFailureMaxPowerMask			= (0x1<<7),
+	
+	outputFailureTimeoutMask			= (0x1<<9),
+	outputCurrentLimitedMask			= (0x1<<10), 
+	outputRampUpMask					= (0x1<<11),
+	outputRampDownMask					= (0x1<<12), 
+	
+	outputEnableKillMask				= (0x1<<13),
+	outputEmergencyOffMask				= (0x1<<14)
+};
+
+#define kiSegHVCardProblemMask (outputFailureMaxTerminalVoltageMask | outputFailureMaxCurrentMask | outputFailureMaxTemperatureMask | outputFailureMaxPowerMask | outputFailureTimeoutMask | outputCurrentLimitedMask)
+
+@interface ORiSegHVCard : ORMPodHVCard <ORHWWizard>
+{
+  @protected
+	unsigned long   dataId;
+    short			hwGoal[16];		//value to send to hw
+    short			target[16];		//input by user
+    float			riseRate;
+	NSMutableDictionary* rdParams[16];
+    int				selectedChannel;
+    float			maxCurrent[16];
+	
+	ORTimeRate*		voltageHistory[16];
+	ORTimeRate*		currentHistory[16];
+    BOOL			shipRecords;
+}
+
+#pragma mark ***Initialization
+- (id) init;
+- (void) dealloc;
+- (void) setUpImage;
+- (void) makeMainController;
+- (BOOL) polarity;
+
+#pragma mark ***Accessors
+- (NSString*) settingsLock;
+- (NSString*) name;
+- (BOOL)	shipRecords;
+- (void)	setShipRecords:(BOOL)aShipRecords;
+- (float)	maxCurrent:(short)chan;
+- (void)	setMaxCurrent:(short)chan withValue:(float)aMaxCurrent;
+- (int)		selectedChannel;
+- (void)	setSelectedChannel:(int)aSelectedChannel;
+- (int)		slotChannelValue:(int)aChannel;
+- (int)		channel:(short)i readParamAsInt:(NSString*)name;
+- (float)	channel:(short)i readParamAsFloat:(NSString*)name;
+- (id)		channel:(short)i readParamAsObject:(NSString*)name;
+- (id)		channel:(short)i readParamAsValue:(NSString*)name;
+- (float)	riseRate;	
+- (void)	setRiseRate:(float)aValue;
+- (int)		hwGoal:(short)chan;	
+- (void)	setHwGoal:(short)chan withValue:(int)aValue;
+- (NSString*) hwGoalString:(short)chan;
+- (int)		target:(short)chan;	
+- (void)	setTarget:(short)chan withValue:(int)aValue;
+- (void)	syncDialog;
+- (void)	commitTargetsToHwGoals;
+- (void)	commitTargetToHwGoal:(short)channel;
+- (NSString*) channelState:(short)channel;
+- (int)		numberChannelsOn;
+- (unsigned long) channelStateMask;
+- (int)		numberChannelsRamping;
+- (int)		numberChannelsWithNonZeroVoltage;
+- (int)		numberChannelsWithNonZeroHwGoal;
+- (BOOL)	channelIsRamping:(short)chan;
+- (unsigned long) failureEvents:(short)channel;
+- (unsigned long) failureEvents;
+- (BOOL) channelInBounds:(short)aChan;
+- (BOOL) isOn:(short)aChannel;
+
+#pragma mark ¥¥¥Data Records
+- (unsigned long) dataId;
+- (void) setDataId: (unsigned long) DataId;
+- (void) setDataIds:(id)assigner;
+- (void) syncDataIdsWith:(id)anotherShaper;
+- (NSDictionary*) dataRecordDescription;
+
+#pragma mark ***Polling
+- (void) updateAllValues;
+- (NSArray*) channelUpdateList;
+- (NSArray*) commonChannelUpdateList;
+- (NSArray*) addChannelNumbersToParams:(NSArray*)someChannelParams;
+- (NSArray*) addChannel:(int)i toParams:(NSArray*)someChannelParams;
+- (void) processReadResponseArray:(NSArray*)response;
+- (void) processSyncResponseArray:(NSArray*)response;
+- (void) processWriteResponseArray:(NSArray*)response;
+
+#pragma mark ¥¥¥Hardware Access
+- (void) loadValues:(short)channel;
+- (void) writeVoltage:(short)channel;
+- (void) writeVoltages;
+- (void) writeMaxCurrents;
+- (void) writeMaxCurrent:(short)channel;
+- (void) writeRiseTime;
+- (void) writeRiseTime:(float)aValue;
+- (void) setPowerOn:(short)channel withValue:(BOOL)aValue;
+- (void) turnChannelOn:(short)channel;
+- (void) turnChannelOff:(short)channel;
+- (void) panicChannel:(short)channel;
+- (void) clearPanicChannel:(short)channel;
+- (void) clearEventsChannel:(short)channel;
+- (void) stopRamping:(short)channel;
+- (void) rampToZero:(short)channel;
+- (void) panic:(short)channel;
+
+- (void) loadAllValues;
+- (void) turnAllChannelsOn;
+- (void) turnAllChannelsOff;
+- (void) panicAllChannels;
+- (void) clearAllPanicChannels;
+- (void) clearAllEventsChannels;
+- (void) stopAllRamping;
+- (void) rampAllToZero;
+- (void) panicAll;
+
+#pragma mark ¥¥¥Trends
+- (ORTimeRate*) voltageHistory:(short)index;
+- (ORTimeRate*) currentHistory:(short)index;
+- (void) shipDataRecords;
+
+#pragma mark ¥¥¥Convenience Methods
+- (float) voltage:(short)aChannel;
+- (float) current:(short)aChannel;
+
+#pragma mark ¥¥¥Archival
+- (id)initWithCoder:(NSCoder*)decoder;
+- (void)encodeWithCoder:(NSCoder*)encoder;
+- (NSMutableDictionary*) addParametersToDictionary:(NSMutableDictionary*)dictionary;
+- (void) addCurrentState:(NSMutableDictionary*)dictionary cIntArray:(short*)anArray forKey:(NSString*)aKey;
+- (void) addCurrentState:(NSMutableDictionary*)dictionary cFloatArray:(float*)anArray forKey:(NSString*)aKey;
+- (void) addCurrentState:(NSMutableDictionary*)dictionary cBoolArray:(BOOL*)anArray forKey:(NSString*)aKey;
+
+#pragma mark ¥¥¥HW Wizard
+- (NSArray*) wizardParameters;
+- (int) numberOfChannels;
+- (NSArray*) wizardSelections;
+
+@end
+
+@interface NSObject (ORiSegHVCard)
+- (BOOL) power;
+@end
+
+extern NSString* ORiSegHVCardShipRecordsChanged;
+extern NSString* ORiSegHVCardMaxCurrentChanged;
+extern NSString* ORiSegHVCardSelectedChannelChanged;
+extern NSString* ORiSegHVCardRiseRateChanged;
+extern NSString* ORiSegHVCardHwGoalChanged;
+extern NSString* ORiSegHVCardTargetChanged;
+extern NSString* ORiSegHVCardCurrentChanged;
+extern NSString* ORiSegHVCardSettingsLock;
+extern NSString* ORiSegHVCardOutputSwitchChanged;
+extern NSString* ORiSegHVCardChannelReadParamsChanged;
