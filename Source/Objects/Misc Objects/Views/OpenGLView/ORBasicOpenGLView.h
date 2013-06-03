1de6d72476cdd2f15dbfb7ab4111355844a25d95
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2013-06-03 19:58:35 +0000

    finally had some time to actually make at least some of this work.
    git-svn-id: svn://orca.physics.unc.edu/Orca@8335 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Misc Objects/Views/OpenGLView/ORBasicOpenGLView.h
+++ Source/Objects/Misc Objects/Views/OpenGLView/ORBasicOpenGLView.h
@@ -1,25 +1,14 @@
-//-----------------------------------------------------------
-//This program was prepared for the Regents of the University of 
-//Washington at the Center for Experimental Nuclear Physics and 
-//Astrophysics (CENPA) sponsored in part by the United States 
-//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
-//The University has certain rights in the program pursuant to 
-//the contract and the program should not be copied or distributed 
-//outside your organization.  The DOE and the University of 
-//Washington reserve all rights in the program. Neither the authors,
-//University of Washington, or U.S. Government make any warranty, 
-//express or implied, or assume any liability or responsibility 
-//for the use of this software.
-//-------------------------------------------------------------
+//This code is a small mod of Apple's BasicOpenGLView example code
 
 
-//#import <Carbon/Carbon.h>
 #import <OpenGL/gl.h>
 #import <OpenGL/glext.h>
 #import <OpenGL/glu.h>
 
+#import "GLString.h"
+
 typedef struct {
-   GLdouble x,y,z;
+    GLdouble x,y,z;
 } recVec;
 
 typedef struct {
@@ -33,53 +22,44 @@ typedef struct {
 
 @interface ORBasicOpenGLView : NSOpenGLView
 {
-	// string attributes
-	IBOutlet id dataSource;
-	NSMutableDictionary * stanStringAttrib;
-	
-	bool fGLInit;
-	
-	NSTimer* timer;
-	
-	CFAbsoluteTime time;
-	
-	// camera handling
+    IBOutlet id delegate;
+	NSMutableDictionary* stanStringAttrib;
 	recCamera camera;
 	GLfloat worldRotation [4];
+	GLfloat objectRotation [4];
 	GLfloat shapeSize;
-	
+    recVec gOrigin;
 }
 
 + (NSOpenGLPixelFormat*) basicPixelFormat;
+
+- (id) initWithFrame: (NSRect) frameRect;
+- (void) awakeFromNib;
+- (BOOL) acceptsFirstResponder;
+- (BOOL) becomeFirstResponder;
+- (BOOL) resignFirstResponder;
 
 - (void) updateProjection;
 - (void) updateModelView;
 - (void) resizeGL;
 - (void) resetCamera;
 
-- (void) mouseDown:(NSEvent *)theEvent;
-- (void) rightMouseDown:(NSEvent *)theEvent;
-- (void) otherMouseDown:(NSEvent *)theEvent;
-- (void) mouseUp:(NSEvent *)theEvent;
-- (void) rightMouseUp:(NSEvent *)theEvent;
-- (void) otherMouseUp:(NSEvent *)theEvent;
-- (void) mouseDragged:(NSEvent *)theEvent;
-- (void) scrollWheel:(NSEvent *)theEvent;
-- (void) rightMouseDragged:(NSEvent *)theEvent;
-- (void) otherMouseDragged:(NSEvent *)theEvent;
+- (void)keyDown:(NSEvent*)theEvent;
+
+- (void) mouseDown:(NSEvent*)theEvent;
+- (void) rightMouseDown:(NSEvent*)theEvent;
+- (void) otherMouseDown:(NSEvent*)theEvent;
+- (void) mouseUp:(NSEvent*)theEvent;
+- (void) rightMouseUp:(NSEvent*)theEvent;
+- (void) otherMouseUp:(NSEvent*)theEvent;
+- (void) mouseDragged:(NSEvent*)theEvent;
+- (void) scrollWheel:(NSEvent*)theEvent;
+- (void) rightMouseDragged:(NSEvent*)theEvent;
+- (void) otherMouseDragged:(NSEvent*)theEvent;
 
 - (void) drawRect:(NSRect)rect;
+- (void) draw3D:(NSRect)rect;
 
 - (void) prepareOpenGL;
-
-- (BOOL) acceptsFirstResponder;
-- (BOOL) becomeFirstResponder;
-- (BOOL) resignFirstResponder;
-
-- (id) initWithFrame: (NSRect) frameRect;
-
 @end
 
-@interface NSObject (ORGenericView)
--(void)drawIn3DView:(NSOpenGLView*)aView;
-@end
