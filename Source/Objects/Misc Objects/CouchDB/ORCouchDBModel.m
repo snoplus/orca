e2dd47dda45145cea92e73db1d4dd9bdff56eae0
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2014-03-22 16:27:16 +0000

    arg — had deleted the time stamp by accident. put it back in
    git-svn-id: svn://orca.physics.unc.edu/Orca@9030 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Misc Objects/CouchDB/ORCouchDBModel.m
+++ Source/Objects/Misc Objects/CouchDB/ORCouchDBModel.m
@@ -1186,10 +1186,31 @@ static NSString* ORCouchDBModelInConnector 	= @"ORCouchDBModelInConnector";
 - (void) addObject:(OrcaObject*)anObj adcDictionary:(NSDictionary*)aDictionary dataBaseRef:(ORCouchDB*)aDataBaseRef
 {
     [self checkDataBaseExists:aDataBaseRef];
+    //these are special records that any object can insert into the database via this notification
+    //the userInfo should just be a dictionary that you want to go into the database
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = @"yyyy/MM/dd HH:mm:ss";
+    
+    NSTimeZone* gmt = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
+    [dateFormatter setTimeZone:gmt];
+    NSString*   lastTimeStamp       = [dateFormatter stringFromDate:[NSDate date]];
+    NSDate*     gmtTime             = [dateFormatter dateFromString:lastTimeStamp];
+    unsigned long secondsSince1970  = [gmtTime timeIntervalSince1970];
+    [dateFormatter release];
     
     NSString* anId = [anObj fullID];
     if([anId length] && aDictionary){
-        [aDataBaseRef addDocument:aDictionary tag:kDocumentAdded];
+        if(![lastTimeStamp length]) lastTimeStamp = @"0";
+        
+        
+        NSMutableDictionary* aRecord = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                        lastTimeStamp,	@"timestamp",
+                                        [NSNumber numberWithUnsignedLong: secondsSince1970],		@"time",
+                                        nil];
+        
+        [aRecord addEntriesFromDictionary:aDictionary];
+        [aDataBaseRef addDocument:aRecord tag:kDocumentAdded];
     }
 }
 
