5110747e1078bd7d39c83d805f70a8825fd6f55d
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2014-04-04 14:03:11 +0000

    fixed an array overrun.
    git-svn-id: svn://orca.physics.unc.edu/Orca@9087 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Hardware/Serial/TPG256A/ORTPG256AModel.m
+++ Source/Objects/Hardware/Serial/TPG256A/ORTPG256AModel.m
@@ -624,7 +624,6 @@ NSString* ORTPG256ALock = @"ORTPG256ALock";
                              [NSString stringWithFormat:@"%.2E",pressure[3]], @"chan 3",
                              [NSString stringWithFormat:@"%.2E",pressure[4]], @"chan 4",
                              [NSString stringWithFormat:@"%.2E",pressure[5]], @"chan 5",
-                             [NSString stringWithFormat:@"%.2E",pressure[6]], @"chan 6",
                              [NSNumber numberWithInt:    pollTime],           @"pollTime",
                             nil];
     [[NSNotificationCenter defaultCenter] postNotificationName:@"ORCouchDBAddObjectRecord" object:self userInfo:values];
