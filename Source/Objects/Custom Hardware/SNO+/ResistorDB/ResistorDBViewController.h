a463b45ddf2d5b0c5a0c775efc85fd8cb0e019ac
Chris Jones <christopher.jones@physics.ox.ac.uk>
2014-04-30 13:14:27 -0400

    continue to flesh out resistordb but weird compile error keeps occuring

--- Source/Objects/Custom Hardware/SNO+/ResistorDB/ResistorDBViewController.h
+++ Source/Objects/Custom Hardware/SNO+/ResistorDB/ResistorDBViewController.h
@@ -10,6 +10,26 @@
 
 @interface ResistorDBViewController : OrcaObjectController {
     
+    IBOutlet NSButton *queryDBButton;
+    IBOutlet NSComboBox *crateSelect;
+    IBOutlet NSComboBox *cardSelect;
+    IBOutlet NSComboBox *channelSelect;
+    IBOutlet NSTextField *currentResistorStatus;
+    IBOutlet NSTextField *currentSNOLowOcc;
+    IBOutlet NSTextField *currentPMTRemoved;
+    IBOutlet NSTextField *currentPMTReinstallled;
+    IBOutlet NSTextField *currentPulledCable;
+    IBOutlet NSTextField *currentBadCable;
+    IBOutlet NSMatrix *updateResistorStatus;
+    IBOutlet NSMatrix *updateSnoLowOcc;
+    IBOutlet NSMatrix *updatePmtRemoved;
+    IBOutlet NSMatrix *updatePmtReinstalled;
+    IBOutlet NSMatrix *updatePulledCable;
+    IBOutlet NSMatrix *updateBadCable;
+    IBOutlet NSComboBox *updateReasonBox;
+    IBOutlet NSTextField *updateReasonOther;
+    IBOutlet NSTextField *updateInfoForPull;
+    IBOutlet NSButton *updateDbButton;
 }
 
 -(id)init;
