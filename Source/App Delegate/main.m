15d6d759578ce06bfb8ee6c0fd428973c8b8878d
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2014-05-09 17:46:00 +0000

    a test to prevent ORCA from ‘napping’ in 10.9
    git-svn-id: svn://orca.physics.unc.edu/Orca@9170 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/App Delegate/main.m
+++ Source/App Delegate/main.m
@@ -25,6 +25,10 @@
 
 int main(int argc, const char *argv[])
 {
+#if defined(MAC_OS_X_VERSION_10_9) && (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_9)
+    [[NSProcessInfo processInfo] beginActivityWithOptions:NSActivityLatencyCritical
+                                                   reason:@"Real-time control/DAQ"];
+#endif
 	//NSZombieEnabled = YES;
 	[NetSocket ignoreBrokenPipes];
 	return  NSApplicationMain(argc, argv);
