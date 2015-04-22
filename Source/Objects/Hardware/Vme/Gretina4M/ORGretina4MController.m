a6b786628fb1806d82474ad37f606483c6c991d5
meijer <meijer@fd86f90f-2c0a-422a-b972-84106d8db31b>
2015-04-22 14:49:03 +0000

    Update of settingsLockedChanged in ORGretina4M
    git-svn-id: svn://orca.physics.unc.edu/Orca@9882 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Hardware/Vme/Gretina4M/ORGretina4MController.m
+++ Source/Objects/Hardware/Vme/Gretina4M/ORGretina4MController.m
@@ -1032,14 +1032,16 @@
     [externalWindowField        setEnabled:!lockedOrRunningMaintenance && !downloading];
     [extTrigLengthField         setEnabled:!lockedOrRunningMaintenance && !downloading];
 
-    [mrpsdvMatrix           setEnabled:!lockedOrRunningMaintenance && !downloading];
-    [mrpsrtMatrix           setEnabled:!lockedOrRunningMaintenance && !downloading];
-    [prerecntMatrix          setEnabled:!lockedOrRunningMaintenance && !downloading];
-    [postrecntMatrix           setEnabled:!lockedOrRunningMaintenance && !downloading];
-    [ftCntMatrix           setEnabled:!lockedOrRunningMaintenance && !downloading];
+    [mrpsdvMatrix       setEnabled:!lockedOrRunningMaintenance && !downloading];
+    [mrpsrtMatrix       setEnabled:!lockedOrRunningMaintenance && !downloading];
+    [prerecntMatrix     setEnabled:!lockedOrRunningMaintenance && !downloading];
+    [postrecntMatrix    setEnabled:!lockedOrRunningMaintenance && !downloading];
+    [ftCntMatrix        setEnabled:!lockedOrRunningMaintenance && !downloading];
 
+    [forceFullInitMatrix        setEnabled:!lockedOrRunningMaintenance && !downloading];
+    [forceFullInitCardButton    setEnabled:!lockedOrRunningMaintenance && !downloading];
     
-    [tpolMatrix             setEnabled:!lockedOrRunningMaintenance && !downloading];
+    [tpolMatrix         setEnabled:!lockedOrRunningMaintenance && !downloading];
     [triggerModeMatrix  setEnabled:!lockedOrRunningMaintenance && !downloading];
     
     [easySetButton      setEnabled:!lockedOrRunningMaintenance && !downloading];
