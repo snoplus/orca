c585ffcd13bd98e894222d21b13b562d0afeb059
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2015-09-17 14:24:28 +0000

    fix compiler warning
    git-svn-id: svn://orca.physics.unc.edu/Orca@10208 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Main Dialogs/MailCenter/ORMailer.m
+++ Source/Main Dialogs/MailCenter/ORMailer.m
@@ -112,8 +112,8 @@
                 people = [cc componentsSeparatedByString:@","];
                 for(id aPerson in people){
                     if([aPerson rangeOfString:@"@"].location != NSNotFound && [aPerson length]>0){
-                        NSDictionary* properties = [NSDictionary dictionaryWithObjectsAndKeys: aPerson, @"address",nil];
-                        MailToRecipient *theRecipient = [[[mail classForScriptingClass:@"cc recipient"] alloc] initWithProperties:properties];
+                        NSDictionary*    properties   = [NSDictionary dictionaryWithObjectsAndKeys: aPerson, @"address",nil];
+                        MailCcRecipient* theRecipient = [[[mail classForScriptingClass:@"cc recipient"] alloc] initWithProperties:properties];
                         [emailMessage.ccRecipients addObject: theRecipient];
                         [theRecipient release];
                     }
