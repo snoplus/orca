24519eade3c244d1450b9591891e7197afff5df9
mgmarino <mgmarino@fd86f90f-2c0a-422a-b972-84106d8db31b>
2015-10-01 09:09:41 +0000

    Improve thread safety of ORVVmeCard.
    git-svn-id: svn://orca.physics.unc.edu/Orca@10258 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Hardware/Vme/VMEcpu/VME_Readout_Code/ORVVmeCard.hh
+++ Source/Objects/Hardware/Vme/VMEcpu/VME_Readout_Code/ORVVmeCard.hh
@@ -3,19 +3,20 @@
 
 #include <set>
 #include "ORVCard.hh"
+#include <pthread.h>
 
 
 class TUVMEDevice;
-class ORVVmeCard : public ORVCard 
+class ORVVmeCard : public ORVCard
 {
    public:
        ORVVmeCard(SBC_card_info* card_info);
-       virtual ~ORVVmeCard(); 
+       virtual ~ORVVmeCard();
 
     protected:
 	/* All the following functions return the bytes written/read,
 	   or -1 if an error has occurred. */
-	
+
        static int32_t DMARead(uint32_t vme_address,
                               uint32_t address_modifier,
                               uint32_t data_width,
@@ -29,7 +30,7 @@ class ORVVmeCard : public ORVCard
                                 uint8_t* buffer,
                                 uint32_t number_of_bytes,
                                 bool auto_increment = true);
-           
+
        static int32_t VMERead(uint32_t vme_address,
                               uint32_t address_modifier,
                               uint32_t data_width,
@@ -70,7 +71,9 @@ class ORVVmeCard : public ORVCard
 
 
     private:
-        static TUVMEDevice* fDevice; 
-        static std::set<ORVCard*> fAllDevices; 
+        typedef std::set<ORVCard*> DeviceSet;
+        static TUVMEDevice* fDevice;
+        static DeviceSet* fAllDevices;
+        static pthread_mutex_t    fMutex;
 };
  #endif /* _ORVVmeCard_hh_*/
