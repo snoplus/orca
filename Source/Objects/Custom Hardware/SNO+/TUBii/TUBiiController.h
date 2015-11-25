9173c4476a8ab6b25c593d09f565a5800537a823
Eric Marzec <marzece@sas.upenn.edu>
2015-11-24 20:24:31 -0500

    Begin caen gui development

--- Source/Objects/Custom Hardware/SNO+/TUBii/TUBiiController.h
+++ Source/Objects/Custom Hardware/SNO+/TUBii/TUBiiController.h
@@ -9,6 +9,12 @@
 #import "OrcaObjectController.h"
 
 @interface TUBiiController : OrcaObjectController {
+    NSView *blankView;
+    IBOutlet NSTabView *tabView;
+    NSSize PulserAndDelays_size;
+    NSSize Triggers_size;
+    NSSize Tubii_size;
+    NSSize Analog_size;
 }
 
 // These references to UI elements are created by CTRL-dragging them into this
