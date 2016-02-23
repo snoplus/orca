6be19c394ff68147041d5ba7c35459ba6e1e023c
Eric Marzec <marzece@sas.upenn.edu>
2016-02-23 10:21:28 -0500

    Moves comments around

--- OrcaTests/NSLogTest.m
+++ OrcaTests/NSLogTest.m
@@ -4,8 +4,11 @@
 //
 //  Created by Eric Marzec (marzece@gmail.com) on 2/22/16.
 //
-//
-
+//  N.B wRML means with Run Main Loop.
+//  I use it in test names to indicate that the main loop
+//  is allowed to run and do as it pleases at some point in
+//  the execution of the test.
+//  This allows for things like GUI updating to occur.
 #import <Cocoa/Cocoa.h>
 #import <Foundation/Foundation.h>
 #import <XCTest/XCTest.h>
@@ -42,28 +45,25 @@
     [statusCont release];
     [super tearDown];
 }
-
+//Simply checks to see that the status controller was retrieved
 - (void)testRetrieveStatusController {
     XCTAssertNotNil(statusCont,@"Could not get status controller");
 }
-
+//Prints a string using the status controller then immediatly
+//checks to see if it shows up
+//The main thread stays in the function the entire time
 - (void)testBasicPrinting {
-    //Prints a string using the status controller then immediatly
-    //checks to see if it shows up
     NSString *testString = @"TEST STRING\n";
     [self printStringToStatusController:testString];
-
     
     NSString* txt =[statusCont contents];
     NSRange range = [txt rangeOfString:testString];
     
     XCTAssert(range.length>0,@"Test string was not printed");
 }
-
+//Prints a string to the status controller then lets the main
+//loop run for a while, then checks if the string is there
 - (void)testBasicPrint_wRML{
-    //wRML = with Run Main Loop
-    //Prints a string to the status controller then lets the main
-    //loop run for a while, then checks if the string is there
     NSString *testString = @"TEST STRING with RML\n";
     [self printStringToStatusController:testString];
     
@@ -73,11 +73,9 @@
     NSRange range = [txt rangeOfString:testString];
     XCTAssert(range.length>0,@"Test string was not printed");
 }
-
+//Prints to in a (non-main) thread then checks if the output
+//shows up. The main loop stays in the function the entire time
 - (void)testSecondaryThreadPrint {
-    //Prints to in a (non-main) thread then checks if the output
-    //shows up. The main loop stays in the function the entire time
-    time_t TimeOutTimer = time(0);
     if (![NSThread isMainThread]) {
         XCTFail(@"Test was not performed on main thread");
         return;
@@ -93,11 +91,9 @@
     NSRange range = [txt rangeOfString:printSingleLineTestString];
     XCTAssert(range.length >0,@"Secondary thread failed to print correctly");
 }
-
+//Same as testSecondaryThreadPrint except a pause is taken to let the
+//main loop run before any checking is done.
 - (void)testSecondaryThreadPrint_wRML {
-    //wRML = with Run Main Loop
-    //Same as testSecondaryThreadPrint except a pause is taken to let the
-    //main loop run before any checking is done.
     if (![NSThread isMainThread]) {
         XCTFail(@"Test was not performed on main thread");
         return;
@@ -114,11 +110,11 @@
     NSRange range = [txt rangeOfString:printSingleLineTestString];
     XCTAssert(range.length >0,@"Secondary thread failed to print correctly");
 }
-
+//This test will print many statements to the status controller and check
+//to make sure they all get logged properly, in the correct order.
+//All this is done within the main thread
+//and the main thread never leaves this function
 - (void)testLotsOfPrinting_MainThread {
-    //This test will print 10000 statements to the status controller and check
-    //to make sure they all get logged properly, in the correct order.
-    //All this is done within the main thread
     if(![NSThread isMainThread]) {
         XCTFail(@"Main thread test not launched on main thread");
     }
@@ -132,11 +128,9 @@
     }
     [self CheckSequentialPrints];
 }
-
+//Same as testLotsOfPrinting_MainThread except a pause is taken to let the
+//main loop run before any checking is done.
 - (void)testLotsOfPrinting_MainThread_wRML {
-    //wRML = with Run Main Loop
-    //Same as testLotsOfPrinting_MainThread except a pause is taken to let the
-    //main loop run before any checking is done.
     if(![NSThread isMainThread]) {
         XCTFail(@"Main thread test not launched on main thread");
     }
@@ -153,25 +147,25 @@
     }
     [self CheckSequentialPrints];
 }
-
+//Runs a separate thread that outputs lots of times then
+//checks after a reasonable time if that output is show.
+//The main loop never leaves this function
 - (void)testLotsOfPrinting_SecondaryThread {
-    //Runs a separate thread that outputs lots of times then
-    //checks after a reasonable time if that output is show.
-    //The main loop never leaves this function
     UInt StartingLength = [[statusCont contents] length];
 
     [NSThread detachNewThreadSelector:@selector(printSequentially) toTarget:self withObject:nil];
     [NSThread sleepForTimeInterval:TimeOut];     //Wait a reasonable amount of time
     NSString *txt = [statusCont contents];
-    XCTAssertGreaterThan([txt length], StartingLength,@"Secondary thread failed to print");
     if([txt length] <= StartingLength) {
+        XCTFail(@"Secondary thread failed to print");
         return;
     }
     [self CheckSequentialPrints];
 }
-
-- (void)testLotsOfPrinting_SecondaryThread_wRML { //RML = Run Main Loop
-    
+//Same as testLotsOfPrinting_SecondarThread() except this test
+//lets the main loop do it's thing for a reasonable amount of time
+//before checking the output.
+- (void)testLotsOfPrinting_SecondaryThread_wRML {
     UInt StartingLength = [[statusCont contents] length];
 
     [NSThread detachNewThreadSelector:@selector(printSequentially) toTarget:self withObject:nil];
@@ -180,19 +174,16 @@
     
     NSString *txt = [statusCont contents];
     if([txt length] <= StartingLength) {
-        //Note I could just use XCTFail here but I don't b/c that doesn't give any useful debugging output
-        XCTAssertGreaterThan([txt length], StartingLength,@"Secondary thread failed to print");
+        XCTFail(@"Secondary thread failed to print");
         return;
     }
-    //Check that every string showed up and are in the correct order
     [self CheckSequentialPrints];
 }
-
+//This test just detects if a deadlock can occur when launching a secondary thread.
+//It doesn't not bother checking if things actually get outputted correctly.
+//So the actual logging could totally fail and this test would still pass
+//so long as it fails without locking up
 - (void)testDeadlock {
-    //This test just detects if a deadlock can occur when launching a secondary thread.
-    //It doesn't not bother checking if things actually get outputted correctly.
-    //So the actual logging could totally fail and this test would still pass
-    //so long as it fails without locking up
     time_t TimeOutTimer = time(0);
     if (![NSThread isMainThread]) {
         XCTFail(@"Test was not performed on main thread");
@@ -212,7 +203,7 @@
     }
     XCTAssert(!sharedVar,"Deadlock occurred"); //If sharedVar is not false it's b/c a timeout/deadlock occurred
 }
-
+//Test that runs some performance metrics printing to the status controller
 -(void)testPerformance {
     NSString *testString = @"performance test string\n";
     NSAttributedString *attrString1 = [[NSAttributedString alloc] initWithString:testString];
@@ -226,15 +217,16 @@
 }
 
 //Helper Functions
+
+//This function is meant to be run on a secondary (non-main) thread.
+//It is supposed to print a test string to the status controller, and then it should return.
+//If printing to the status controller blocks this function will not return until that block is resolved.
 - (void)printSingleLine {
-    //This function is meant to be run on a secondary (non-main) thread.
-    //It is supposed to print a test string to the status controller, and then it should return.
-    //If printing to the status controller blocks this function will not return until that block is resolved.
     [self printStringToStatusController:printSingleLineTestString];
     sharedVar = NO; //Change this so other threads know this function is done
 }
+//Prints many line of text which can be checked to make sure they show up in the correct order.
 - (void)printSequentially {
-    //Prints many line of text which can be checked to make sure they show up in the correct order.
     for (uint i=0; i < nPrints; i++) {
         NSString* testString = [NSString stringWithFormat:printSeqTestString,i];
         [self printStringToStatusController:testString];
@@ -242,19 +234,20 @@
         sharedVar = NO; //Change this so other threads know this function is done
     }
 }
+
 - (void)printStringToStatusController:(NSString*)str {
     NSAttributedString *attrString = [[NSAttributedString alloc] initWithString:str];
     [statusCont printAttributedString:attrString];
     [attrString release];
 }
+//This function just lets the main loop run for a few seconds (or whatever TimeOut is set to)
 - (void)RunMainLoop {
-    //This function just lets the main loop run for a few seconds (or whatever TimeOut is set to)
     NSDate *date = [[NSDate alloc]initWithTimeIntervalSinceNow:TimeOut];
     [[NSRunLoop mainRunLoop] runUntilDate:date];
     [date release];
 }
+//This function is used to check that the output for printSequentially actually showed up.
 - (void) CheckSequentialPrints {
-    //This function is used to check that the output for printSequentially actually showed up.
     NSString* txt = [statusCont contents];
     for(uint i=0;i< nPrints-1;i++)
     {
