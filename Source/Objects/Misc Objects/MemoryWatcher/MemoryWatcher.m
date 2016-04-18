c85f688498127a51df423818ef7c4131e1acfe8e
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2016-04-18 13:56:40 +0000

    removed the fileClose. Not needed. -- might be needed for  OS <10.11
    git-svn-id: svn://orca.physics.unc.edu/Orca@10558 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Misc Objects/MemoryWatcher/MemoryWatcher.m
+++ Source/Objects/Misc Objects/MemoryWatcher/MemoryWatcher.m
@@ -4,16 +4,16 @@
 //  Created by Mark Howe on Friday 05/13/2005.
 //  Copyright (c) 2005 CENPA, University of Washington. All rights reserved.
 //-----------------------------------------------------------
-//This program was prepared for the Regents of the University of 
-//Washington at the Center for Experimental Nuclear Physics and 
-//Astrophysics (CENPA) sponsored in part by the United States 
-//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
-//The University has certain rights in the program pursuant to 
-//the contract and the program should not be copied or distributed 
-//outside your organization.  The DOE and the University of 
+//This program was prepared for the Regents of the University of
+//Washington at the Center for Experimental Nuclear Physics and
+//Astrophysics (CENPA) sponsored in part by the United States
+//Department of Energy (DOE) under Grant #DE-FG02-97ER41020.
+//The University has certain rights in the program pursuant to
+//the contract and the program should not be copied or distributed
+//outside your organization.  The DOE and the University of
 //Washington reserve all rights in the program. Neither the authors,
-//University of Washington, or U.S. Government make any warranty, 
-//express or implied, or assume any liability or responsibility 
+//University of Washington, or U.S. Government make any warranty,
+//express or implied, or assume any liability or responsibility
 //for the use of this software.
 //-------------------------------------------------------------
 
@@ -34,15 +34,15 @@ NSString* MemoryWatcherChangedNotification   = @"MemoryWatcherChangedNotificatio
 NSString* MemoryWatcherTaskIntervalNotification = @"MemoryWatcherTaskIntervalNotification";
 
 enum {
-	kCPU,
-	kRSize,
-	kVSize
+    kCPU,
+    kRSize,
+    kVSize
 };
 
 @implementation MemoryWatcher
 
 #pragma mark ***Initialization
-- (id) init 
+- (id) init
 {
     self = [super init];
     [self setTaskInterval:3*60];
@@ -51,7 +51,7 @@ enum {
     return self;
 }
 
-- (void) dealloc 
+- (void) dealloc
 {
     [launchTime release];
     [NSObject cancelPreviousPerformRequestsWithTarget:self];
@@ -61,18 +61,18 @@ enum {
         [vmTask terminate];
     }
     [vmTask release];
-	
-	int i;
-	for(i=0;i<kNumWatchedValues;i++){
-		[timeRate[i] release];
-	}
+    
+    int i;
+    for(i=0;i<kNumWatchedValues;i++){
+        [timeRate[i] release];
+    }
     [super dealloc];
 }
 
 #pragma mark ***Accessors
 - (NSTimeInterval) accurateUptime
 {
-	return [[NSDate date] timeIntervalSinceDate:launchTime]; 
+    return [[NSDate date] timeIntervalSinceDate:launchTime];
 }
 
 
@@ -109,12 +109,12 @@ enum {
 {
     if(aTaskInterval<1)aTaskInterval = 1;
     taskInterval = aTaskInterval;
-	
-	[[NSNotificationCenter defaultCenter]
-				postNotificationName:MemoryWatcherTaskIntervalNotification
-							  object:self];
-
-
+    
+    [[NSNotificationCenter defaultCenter]
+     postNotificationName:MemoryWatcherTaskIntervalNotification
+     object:self];
+    
+    
 }
 
 - (NSMutableString*) taskResult
@@ -125,7 +125,7 @@ enum {
 - (void) setTaskResult:(NSMutableString*)aTaskResult
 {
     [taskResult autorelease];
-    taskResult = [aTaskResult mutableCopy];    
+    taskResult = [aTaskResult mutableCopy];
 }
 
 - (NSTask*) vmTask
@@ -142,41 +142,41 @@ enum {
 
 - (unsigned) timeRateCount:(int)rateIndex
 {
-	if(rateIndex<kNumWatchedValues)return [timeRate[rateIndex] count];
-	else return 0;
+    if(rateIndex<kNumWatchedValues)return [timeRate[rateIndex] count];
+    else return 0;
 }
 
 - (float) timeRate:(int)rateIndex value:(int)valueIndex
-{	
-	if(rateIndex<kNumWatchedValues)return [timeRate[rateIndex] valueAtIndex:valueIndex];
-	else return 0;
+{
+    if(rateIndex<kNumWatchedValues)return [timeRate[rateIndex] valueAtIndex:valueIndex];
+    else return 0;
 }
 
 - (void) launchTask
 {
     if(![vmTask isRunning]){
         
-		int i;
-		for(i=0;i<kNumWatchedValues;i++){
+        int i;
+        for(i=0;i<kNumWatchedValues;i++){
             if(!timeRate[i]){
                 timeRate[i] = [[ORTimeRate alloc] init];
                 [timeRate[i] setSampleTime:taskInterval];
             }
-		}
+        }
         [self setTaskResult:[NSMutableString string]];
-		
+        
         NSTask *t = [[NSTask alloc] init];
         [self setVmTask:t];
         [t release];
-        		
+        
         NSPipe *newPipe = [NSPipe pipe];
         NSFileHandle *readHandle = [newPipe fileHandleForReading];
         
         NSNotificationCenter* nc = [NSNotificationCenter defaultCenter];
         
-        [nc addObserver:self 
-               selector:@selector(taskDataAvailable:) 
-                   name:NSFileHandleReadCompletionNotification 
+        [nc addObserver:self
+               selector:@selector(taskDataAvailable:)
+                   name:NSFileHandleReadCompletionNotification
                  object:readHandle];
         
         [nc addObserver : self
@@ -193,14 +193,13 @@ enum {
         [vmTask launch];
     }
     
-    [self setUpTime:[[NSDate date] timeIntervalSinceDate:launchTime]]; 
+    [self setUpTime:[[NSDate date] timeIntervalSinceDate:launchTime]];
     
 }
 
 - (void) taskCompleted: (NSNotification*)aNote
 {
     if([aNote object] == vmTask){
-        //[[[vmTask standardOutput] fileHandleForReading] closeFile];
         [self setVmTask:nil];
         [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(launchTask) object:nil];
         [self performSelector:@selector(launchTask) withObject:nil afterDelay:taskInterval];
@@ -209,29 +208,26 @@ enum {
 
 - (void) taskDataAvailable:(NSNotification*)aNotification
 {
-	NSData* incomingData   = [[aNotification userInfo] valueForKey:NSFileHandleNotificationDataItem];
-	if(incomingData){
-		NSString *incomingText = [[NSString alloc] initWithData:incomingData encoding:NSASCIIStringEncoding];
-		[taskResult appendString:incomingText];
-		[incomingText release];
-	}
+    NSData* incomingData   = [[aNotification userInfo] valueForKey:NSFileHandleNotificationDataItem];
+    if (incomingData && [incomingData length]) {
+        NSString *incomingText = [[NSString alloc] initWithData:incomingData encoding:NSASCIIStringEncoding];
+        [taskResult appendString:incomingText];
+        [incomingText release];
+        [[aNotification object] readInBackgroundAndNotify];  // go back for more.
+    }
+    
+    if(![vmTask isRunning]) {
+        
+        [self processResult];
 
-	if(![vmTask isRunning]) {
-		[self processResult];
-
-		[[NSNotificationCenter defaultCenter] removeObserver:self name:nil object:nil];
-		[taskResult release];
-		taskResult = nil;
-
-		[NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(launchTask) object:nil];
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:nil object:nil];
+        [taskResult release];
+        taskResult = nil;
+        
+        
+        [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(launchTask) object:nil];
         [self performSelector:@selector(launchTask) withObject:nil afterDelay:taskInterval];
-
-
-	}
-	
-
-	
-	[[aNotification object] readInBackgroundAndNotify];  // go back for more.
+    }
 }
 
 
@@ -245,46 +241,46 @@ enum {
 - (void) processResult
 {
     NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-	NSArray* lines = [taskResult componentsSeparatedByString:@"\n"];
-	NSString* goodLine = @"";
-	for(id aLine in lines){
-		if([aLine rangeOfString:@"Orca "].location != NSNotFound){
-			aLine = [aLine removeExtraSpaces];
-			NSArray* parts = [aLine componentsSeparatedByString:@" "];
-			if([parts count]==4 && [[parts objectAtIndex:1] floatValue]){
-				goodLine = aLine;
-				break;
-			}
-		}
-	}
-	if([goodLine length]){
-		//should now have a line that looks like "Orca 0.2 40M+ 1056M"
-		NSScanner* scanner = [[NSScanner alloc] initWithString:goodLine];
-		if([scanner scanUpToCharactersFromSet:[NSCharacterSet decimalDigitCharacterSet] intoString:nil]){
-			float value;
-			//scan in cpu
-			[scanner scanFloat:&value];
-			[timeRate[kCPU] addDataToTimeAverage:value];
-			
-			//scan in RSIZE
-			NSString* multiplier;
-			[scanner scanUpToCharactersFromSet:[NSCharacterSet decimalDigitCharacterSet] intoString:nil];
-			[scanner scanFloat:&value];
-			if([scanner scanUpToCharactersFromSet:[NSCharacterSet whitespaceCharacterSet] intoString:&multiplier]){
-				[timeRate[kRSize] addDataToTimeAverage:[self convertValue:value withMultiplier:multiplier]];
-			}
-			
-			//scan in VSIZE
-			[scanner scanUpToCharactersFromSet:[NSCharacterSet decimalDigitCharacterSet] intoString:nil];
-			[scanner scanFloat:&value];
-			if([scanner scanUpToCharactersFromSet:[NSCharacterSet whitespaceCharacterSet] intoString:&multiplier]){
-				[timeRate[kVSize] addDataToTimeAverage:[self convertValue:value withMultiplier:multiplier]];
-			}
-			
-			[[NSNotificationCenter defaultCenter] postNotificationName:MemoryWatcherChangedNotification object:self];
-		}
-		[scanner release];
-	}
+    NSArray* lines = [taskResult componentsSeparatedByString:@"\n"];
+    NSString* goodLine = @"";
+    for(id aLine in lines){
+        if([aLine rangeOfString:@"Orca "].location != NSNotFound){
+            aLine = [aLine removeExtraSpaces];
+            NSArray* parts = [aLine componentsSeparatedByString:@" "];
+            if([parts count]==4){
+                goodLine = aLine;
+                break;
+            }
+        }
+    }
+    if([goodLine length]){
+        //should now have a line that looks like "Orca 0.2 40M+ 1056M"
+        NSScanner* scanner = [[NSScanner alloc] initWithString:goodLine];
+        if([scanner scanUpToCharactersFromSet:[NSCharacterSet decimalDigitCharacterSet] intoString:nil]){
+            float value;
+            //scan in cpu
+            [scanner scanFloat:&value];
+            [timeRate[kCPU] addDataToTimeAverage:value];
+            
+            //scan in RSIZE
+            NSString* multiplier;
+            [scanner scanUpToCharactersFromSet:[NSCharacterSet decimalDigitCharacterSet] intoString:nil];
+            [scanner scanFloat:&value];
+            if([scanner scanUpToCharactersFromSet:[NSCharacterSet whitespaceCharacterSet] intoString:&multiplier]){
+                [timeRate[kRSize] addDataToTimeAverage:[self convertValue:value withMultiplier:multiplier]];
+            }
+            
+            //scan in VSIZE
+            [scanner scanUpToCharactersFromSet:[NSCharacterSet decimalDigitCharacterSet] intoString:nil];
+            [scanner scanFloat:&value];
+            if([scanner scanUpToCharactersFromSet:[NSCharacterSet whitespaceCharacterSet] intoString:&multiplier]){
+                [timeRate[kVSize] addDataToTimeAverage:[self convertValue:value withMultiplier:multiplier]];
+            }
+            
+            [[NSNotificationCenter defaultCenter] postNotificationName:MemoryWatcherChangedNotification object:self];
+        }
+        [scanner release];
+    }
     [pool release];
 }
 
