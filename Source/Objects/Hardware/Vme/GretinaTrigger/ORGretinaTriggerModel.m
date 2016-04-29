0a2419c1dd9f0e7381d9d0f31d198d548f1cc20b
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2016-04-29 18:39:26 +0000

    no longer reset the clock at the end of the run. Still send the trigger card packet tho
    git-svn-id: svn://orca.physics.unc.edu/Orca@10592 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Hardware/Vme/GretinaTrigger/ORGretinaTriggerModel.m
+++ Source/Objects/Hardware/Vme/GretinaTrigger/ORGretinaTriggerModel.m
@@ -542,18 +542,16 @@ static GretinaTriggerStateInfo router_state_info[kNumRouterTriggerStates] = {
         ORRunModel* aRunModel = [runModelObjects objectAtIndex:0];
         if([aRunModel quickStart]){
             [self shipDataRecord];
-            [ORTimer delay:.1];
-            NSLog(@"Reset the timestamps\n");
-            //to reset the clocks set bit 6 of the MISC_CTRL reg
-            //When this bit is set, the timestamp counter is held reset with value of zero
-            //Since there is just a couple of operations here and we want to be fast just
-            //send the commands without going thru a state machine.
-            unsigned long aValue = [self readRegister:kMiscCtl1];
-            [self writeRegister:kMiscCtl1 withValue:aValue &= ~(0x1<<6)];//release
-            [self resetScaler];
-            [self writeRegister:kPulsedCtl2 withValue:0x1000]; //send one imp syn
-            [self readDisplayRegs];
-            [ORTimer delay:.1];
+//            NSLog(@"Reset the timestamps\n");
+//            //to reset the clocks set bit 6 of the MISC_CTRL reg
+//            //When this bit is set, the timestamp counter is held reset with value of zero
+//            //Since there is just a couple of operations here and we want to be fast just
+//            //send the commands without going thru a state machine.
+//            unsigned long aValue = [self readRegister:kMiscCtl1];
+//            [self writeRegister:kMiscCtl1 withValue:aValue &= ~(0x1<<6)];//release
+//            [self resetScaler];
+//            [self writeRegister:kPulsedCtl2 withValue:0x1000]; //send one imp syn
+//            [self readDisplayRegs];
         }
     }
 
