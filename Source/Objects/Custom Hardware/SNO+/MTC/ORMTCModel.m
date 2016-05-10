da29998a4965ea5a12f92552de541b4705749901
Anthony LaTorre <tlatorre@uchicago.edu>
2016-05-10 17:38:33 -0400

    fix the ordering of the MTCA dac thresholds

--- Source/Objects/Custom Hardware/SNO+/MTC/ORMTCModel.m
+++ Source/Objects/Custom Hardware/SNO+/MTC/ORMTCModel.m
@@ -1760,11 +1760,23 @@ resetFifoOnStart = _resetFifoOnStart;
     int i;
     uint16_t dacs[14];
 
-    @try {
-        for (i = 0; i < 14 ; i++) {
-            dacs[i] = [self dacValueByIndex:i];
-        }
+    dacs[0] = [self dbIntbyIndex:kNHit100LoThreshold];
+    dacs[1] = [self dbIntbyIndex:kNHit100MedThreshold];
+    dacs[2] = [self dbIntbyIndex:kNHit100HiThreshold];
+    dacs[3] = [self dbIntbyIndex:kNHit20Threshold];
+    dacs[4] = [self dbIntbyIndex:kNHit20LBThreshold];
+    dacs[5] = [self dbIntbyIndex:kESumLowThreshold];
+    dacs[6] = [self dbIntbyIndex:kESumHiThreshold];
+    dacs[7] = [self dbIntbyIndex:kOWLNThreshold];
+    dacs[8] = [self dbIntbyIndex:kOWLELoThreshold];
+    dacs[9] = [self dbIntbyIndex:kOWLEHiThreshold];
 
+    /* Last four DAC values are spares? */
+    for (i = 10; i < 14; i++) {
+	dacs[i] = 0;
+    }
+
+    @try {
         [mtc okCommand:"load_mtca_dacs %d %d %d %d %d %d %d %d %d %d %d %d %d %d", dacs[0], dacs[1], dacs[2], dacs[3], dacs[4], dacs[5], dacs[6], dacs[7], dacs[8], dacs[9], dacs[10], dacs[11], dacs[12], dacs[13]];
     } @catch(NSException* e) {
         NSLog(@"failed to load the MTCA dacs: %@", [e reason]);     
