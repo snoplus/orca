14877d02f5a380a3740f82f2042a94dae298dd13
Phil Harvey <phil@work3.phy.queensu.ca>
2016-12-08 13:49:22 -0500

    Updated some comments

--- Source/Objects/Misc Objects/PostgreSQL/ORPQModel.h
+++ Source/Objects/Misc Objects/PostgreSQL/ORPQModel.h
@@ -1,20 +1,23 @@
 //-------------------------------------------------------------------------
 //  ORPQModel.h
 //
-//  Created by Mark A. Howe on Wednesday 10/18/2006.
-//  Copyright (c) 2006 CENPA, University of Washington. All rights reserved.
-//-----------------------------------------------------------
-//This program was prepared for the Regents of the University of 
-//Washington at the Center for Experimental Nuclear Physics and 
-//Astrophysics (CENPA) sponsored in part by the United States 
-//Department of Energy (DOE) under Grant #DE-FG02-97ER41020. 
-//The University has certain rights in the program pursuant to 
-//the contract and the program should not be copied or distributed 
-//outside your organization.  The DOE and the University of 
-//Washington reserve all rights in the program. Neither the authors,
-//University of Washington, or U.S. Government make any warranty, 
-//express or implied, or assume any liability or responsibility 
-//for the use of this software.
+//  2016-06-01 Created by Phil Harvey (Based on ORSqlModel.h by M.Howe)
+//
+//
+//  Abritrary database accesses may be made via this object by defining
+//  a callback function in the calling object like this:
+//
+//    - (void) callbackProc:(ORPQResult*)theResult
+//    {
+//        // do stuff here
+//    }
+//
+//  then calling dbQuery like this:
+//
+//    if ([ORPQModel getCurrent]) {
+//        [[ORPQModel getCurrent] dbQuery:@"<query string>" object:self selector:@selector(callbackProc:)];
+//    }
+//
 //-------------------------------------------------------------
 
 #pragma mark ***Imported Files
