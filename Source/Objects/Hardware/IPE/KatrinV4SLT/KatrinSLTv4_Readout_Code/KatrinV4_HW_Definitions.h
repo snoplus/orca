851518cf55dd81ee4141001779a0703e34e68107
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2017-07-17 14:12:50 +0000

    dev commit
    git-svn-id: svn://orca.physics.unc.edu/Orca@11222 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Hardware/IPE/KatrinV4SLT/KatrinSLTv4_Readout_Code/KatrinV4_HW_Definitions.h
+++ Source/Objects/Hardware/IPE/KatrinV4SLT/KatrinSLTv4_Readout_Code/KatrinV4_HW_Definitions.h
@@ -1,5 +1,5 @@
 //
-// SLTv4_HW_Definitions.h
+// Katrinv4_HW_Definitions.h
 //  Orca
 //
 //  Created by Mark Howe on Mon Mar 10, 2008
@@ -17,8 +17,8 @@
 //express or implied, or assume any liability or responsibility 
 //for the use of this software.
 //-------------------------------------------------------------
-#ifndef _H_SLTV4HWDEFINITIONS_
-#define _H_SLTV4HWDEFINITIONS_
+#ifndef _H_KATRINV4HWDEFINITIONS_
+#define _H_KATRINV4HWDEFINITIONS_
 
 #define kKatrinV4SLT    1
 #define kSLTv4    1
@@ -28,28 +28,27 @@
 #define kReadWaveForms	0x1 << 0
 
 //flt run modes (sent to hw, FLTv4 control register) -> for fltRunMode -tb-
-#define kIpeFltV4Katrin_StandBy_Mode		0
-#define kIpeFltV4Katrin_Run_Mode			1
-#define kIpeFltV4Katrin_Histo_Mode			2
-#define kIpeFltV4Katrin_Veto_Mode			3
-#define kIpeFltV4Katrin_Bipolar_Mode		5
+#define kKatrinV4FLT_StandBy_Mode		0   //no readout
+#define kKatrinV4FLT_Run_Mode			1
+#define kKatrinV4FLT_Histo_Mode			2
+#define kKatrinV4FLT_Veto_Mode			3
+#define kKatrinV4FLT_Bipolar_Mode		5
 
-enum daqMode { 
-	kIpeFltV4_EnergyDaqMode			= 0,
-	kIpeFltV4_EnergyTraceDaqMode	= 1,
-	kIpeFltV4_Histogram_DaqMode		= 2,
-	kIpeFltV4_VetoEnergyDaqMode		= 3, 
-	kIpeFltV4_VetoEnergyTraceDaqMode= 4,
-	kIpeFltV4_VetoEnergyAutoDaqMode = 5, //for future use
-	kIpeFltV4_VetoEnergyTraceSyncDaqMode	= 6, //for future use
-	kIpeFltV4_EnergyTraceSyncDaqMode= 7,
-	kIpeFltV4_BipolarEnergyDaqMode  = 8,
-	kIpeFltV4_BipolarEnergyTraceDaqMode  = 9,
-	kIpeFltV4_NumberOfDaqModes // do not assign a value, the compiler will do it
+enum katrinDaqMode {
+	kKatrinV4Flt_EnergyDaqMode              = 0,
+	kKatrinV4Flt_EnergyTraceDaqMode         = 1,
+	kKatrinV4Flt_Histogram_DaqMode          = 2,
+	kKatrinV4Flt_VetoEnergyDaqMode          = 3,
+	kKatrinV4Flt_VetoEnergyTraceDaqMode     = 4,
+	//kKatrinV4Flt_VetoEnergyAutoDaqMode      = 5, //for future use
+	//kKatrinV4Flt_VetoEnergyTraceSyncDaqMode	= 6, //for future use
+	//kKatrinV4Flt_EnergyTraceSyncDaqMode     = 7,
+	kKatrinV4Flt_BipolarEnergyDaqMode       = 8,
+	kKatrinV4Flt_BipolarEnergyTraceDaqMode  = 9,
+	kKatrinV4Flt_NumberOfDaqModes // do not assign a value, the compiler will do it
 };
 
 //flags in the runFlagsMask, sent to PrPMC by ORIpeV4FLTModel::load_HW_Config_Structure
-#define kFirstTimeFlag              0x010000
 #define kSyncFltWithSltTimerFlag    0x020000
 #define kShipSumHistogramFlag		0x040000
 #define kSecondsSetInitWithHostFlag	0x080000
@@ -57,6 +56,7 @@ enum daqMode {
 //#define kSkipFltEventReadoutFlag	0x200000
 #define kForceFltReadoutFlag	    0x200000
 
+//not used in the newer firmware readout code -MAH- 7/3/2017
 typedef struct { // -tb- 2008-02-27
 	int32_t readoutSec;
 	int32_t refreshTimeSec;  //! this holds the refresh time -tb-
@@ -68,7 +68,7 @@ typedef struct { // -tb- 2008-02-27
     int32_t offsetEMin;
     uint32_t histogramID;
     uint32_t histogramInfo;
-} katrinV4HistogramDataStruct;
+} katrinV4FltHistogramDataStruct;
 
 
 typedef struct { // -tb- 2013-05-27 struct for histogram buffer (for summing up histograms)
@@ -85,7 +85,7 @@ typedef struct { // -tb- 2013-05-27 struct for histogram buffer (for summing up 
     uint32_t histogramID;
     uint32_t histogramInfo;
     uint32_t h[2048];
-} katrinV4FullHistogramDataStruct;
+} katrinV4FltFullHistogramDataStruct;
 
 
 #endif
