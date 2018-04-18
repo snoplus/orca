36f97c43ca71d85c746fcdddeb4849646d8dd198
tlatorre <tlatorre@fd86f90f-2c0a-422a-b972-84106d8db31b>
2018-04-18 14:52:31 +0000

    SNO+: bump SNO+ version number to 1.0.0
    Author: Anthony LaTorre <tlatorre@uchicago.edu>
    
    git-svn-id: svn://orca.physics.unc.edu/Orca@11625 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Experiments/SNOP/SNOPModel.h
+++ Source/Experiments/SNOP/SNOPModel.h
@@ -45,7 +45,7 @@
 #define kNumTubes	20 //XL3s
 #define kNumOfCrates 19 //number of Crates in SNO+
 #define STANDARD_RUN_VERSION 2 //Increase if Standard Runs table structure is changed
-#define SNOP_ORCA_VERSION "0.12.3" //The current Orca release
+#define SNOP_ORCA_VERSION "1.0.0" //The current Orca release
 
 BOOL isNotRunningOrIsInMaintenance();
 
