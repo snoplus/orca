ec02ba59e4dd410f4314fb9ae1968f4e26326ecf
markhowe <markhowe@fd86f90f-2c0a-422a-b972-84106d8db31b>
2018-07-02 14:42:23 +0000

    changed userInfo type to NSDictionary*
    git-svn-id: svn://orca.physics.unc.edu/Orca@11839 fd86f90f-2c0a-422a-b972-84106d8db31b

--- Source/Objects/Hardware/IPE/IPE FLT/ORIpeFLTController.h
+++ Source/Objects/Hardware/IPE/IPE FLT/ORIpeFLTController.h
@@ -148,7 +148,7 @@
 - (IBAction) writeThresholdsGains:(id)sender;
 - (IBAction) calibrateAction:(id)sender;
 #if !defined(MAC_OS_X_VERSION_10_10) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_10 // 10.10-specific
-- (void) calibrationSheetDidEnd:(id)sheet returnCode:(int)returnCode contextInfo:(id)userInfo;
+- (void) calibrationSheetDidEnd:(id)sheet returnCode:(int)returnCode contextInfo:(NSDictionary*)userInfo;
 #endif
 #pragma mark ¥¥¥Plot DataSource
 - (int) numberPointsInPlot:(id)aPlotter;
--- Source/Objects/Hardware/IPE/IPE SLT/ORIpeSLTController.m
+++ Source/Objects/Hardware/IPE/IPE SLT/ORIpeSLTController.m
@@ -46,7 +46,7 @@ NSString* fltTriggerSourceNames[2][kFltNumberTriggerSources] = {
 
 #if !defined(MAC_OS_X_VERSION_10_10) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_10 // 10.10-specific
 @interface ORIpeSLTController (private)
-- (void) calibrationSheetDidEnd:(id)sheet returnCode:(int)returnCode contextInfo:(id)userInfo;
+- (void) calibrationSheetDidEnd:(id)sheet returnCode:(int)returnCode contextInfo:(NSDictionary*)userInfo;
 @end
 #endif
 @implementation ORIpeSLTController
@@ -962,7 +962,7 @@ NSString* fltTriggerSourceNames[2][kFltNumberTriggerSources] = {
 
 #if !defined(MAC_OS_X_VERSION_10_10) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_10 // 10.10-specific
 @implementation ORIpeSLTController (private)
-- (void) calibrationSheetDidEnd:(id)sheet returnCode:(int)returnCode contextInfo:(id)userInfo
+- (void) calibrationSheetDidEnd:(id)sheet returnCode:(int)returnCode contextInfo:(NSDictionary*)userInfo
 {
     if(returnCode == NSAlertAlternateReturn){
 		@try {
